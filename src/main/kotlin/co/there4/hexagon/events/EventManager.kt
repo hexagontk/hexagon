5f2f2fbfea3d849cde79d33e111e0795757a24da
jamming <juanjoaguililla@gmail.com>
2016-04-24 10:17:58 +0200

    Clean up EventManager

--- src/main/kotlin/co/there4/hexagon/events/EventManager.kt
+++ src/main/kotlin/co/there4/hexagon/events/EventManager.kt
@@ -6,15 +6,15 @@ import co.there4.hexagon.util.CompanionLogger
 import kotlin.reflect.KClass
 
 object EventManager : CompanionLogger (EventManager::class) {
-    val EXCHANGE = "events"
+    private val exchange = "events"
 
     val client = RabbitClient ("amqp://guest:guest@localhost")
 
     fun <T : Event> on(type: KClass<T>, event: String, consumer: (T) -> Unit) {
-        client.consume(EXCHANGE, event, type) { consumer(it) }
+        client.consume(exchange, event, type) { consumer(it) }
     }
 
     fun publish(event: Event) {
-        client.publish(EXCHANGE, event.action, event.serialize())
+        client.publish(exchange, event.action, event.serialize())
     }
 }
