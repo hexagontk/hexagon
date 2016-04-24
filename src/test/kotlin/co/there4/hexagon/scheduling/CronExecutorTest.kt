38f5b10d7cf521d73736e0a1953e8b912e186ead
jamming <juanjoaguililla@gmail.com>
2016-04-24 10:31:41 +0200

    Add scheduling test coverage

--- src/test/kotlin/co/there4/hexagon/scheduling/CronExecutorTest.kt
+++ src/test/kotlin/co/there4/hexagon/scheduling/CronExecutorTest.kt
@@ -1,21 +1,33 @@
 package co.there4.hexagon.scheduling
 
 import org.testng.annotations.Test
+import java.lang.Thread.sleep
 
 @Test class CronExecutorTest {
-    private var count = 0
+    fun callback_is_executed_properly () {
+        val cron = CronExecutor ()
+        val times = 1
+        var count = 0
+
+        cron.schedule("0/1 * * * * *") { count++ }
+
+        sleep ((times * 1000) + 100L)
+        cron.shutdown()
+        assert (count == times)
+    }
 
     fun failing_callback_does_not_prevent_future_executions () {
         val cron = CronExecutor ()
         val times = 2
+        var count = 0
 
         cron.schedule("0/1 * * * * *") {
             count++
             throw RuntimeException ("Error in cron job")
         }
 
-        Thread.sleep ((times * 1000) + 100L)
+        sleep ((times * 1000) + 100L)
         cron.shutdown()
-        assert (count == 2)
+        assert (count == times)
     }
 }
