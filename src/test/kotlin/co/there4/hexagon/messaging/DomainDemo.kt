d8e9e15c43cbd37664fa7baff8daaff77d38bd9f
jamming <juanjoaguililla@gmail.com>
2016-04-27 15:51:38 +0200

    Make DSL changes

--- src/test/kotlin/co/there4/hexagon/messaging/DomainDemo.kt
+++ src/test/kotlin/co/there4/hexagon/messaging/DomainDemo.kt
@@ -1,8 +1,8 @@
 package co.there4.hexagon.messaging
 
 class Rabbit {
-    fun exchange (cb: Exchange.() -> Unit) {}
-    fun queue (cb: Exchange.() -> Unit) {}
+    fun exchange (name: String, cb: Exchange.() -> Unit) {}
+    fun queue (name: String, cb: Exchange.() -> Unit) {}
 
 }
 
@@ -10,21 +10,24 @@ class Exchange {
     fun route(rk: String, cb: String.() -> Unit) {}
 }
 
-class Queue
-
-fun rabbit (uri: String, cb: Rabbit.() -> Unit) {
+class Queue {
 
 }
 
+fun rabbit (uri: String, cb: Rabbit.() -> Unit) {}
+
 fun demo () {
     rabbit (uri = "") {
-        exchange {
+        queue("q") {
+
+        }
+        exchange("exchange") {
             route ("evt.routing") {
-                exchange {
+                exchange ("") {
 
                 }
 
-                queue {
+                queue ("") {
 
                 }
 
