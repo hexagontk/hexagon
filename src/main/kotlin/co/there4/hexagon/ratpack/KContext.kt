01f06f1ec021681aee0258c102ddac24f01f874d
jamming <juanjoaguililla@gmail.com>
2016-04-28 18:31:38 +0200

    Context changes
    * Setting content-type depending on template extension
    * Response utility methods

--- src/main/kotlin/co/there4/hexagon/ratpack/KContext.kt
+++ src/main/kotlin/co/there4/hexagon/ratpack/KContext.kt
@@ -1,13 +1,11 @@
 package co.there4.hexagon.ratpack
 
 import co.there4.hexagon.template.PebbleRenderer
+import ratpack.file.MimeTypes
 import ratpack.handling.ByMethodSpec
 import ratpack.handling.Context
 import java.util.*
 
-/**
- * TODO Support setting content-type depending on template extension
- */
 class KContext (private val delegate: Context) : Context by delegate {
     fun byMethod (cb: ByMethodSpec.() -> Unit) {
         delegate.byMethod { it.(cb)() }
@@ -18,10 +16,26 @@ class KContext (private val delegate: Context) : Context by delegate {
         locale: Locale = Locale.getDefault(),
         context: Map<String, *> = mapOf<String, Any> ()) {
 
+        val contentType = get(MimeTypes::class.java).getContentType(template) ?: "text/html"
+        response.headers["Content-Type"] = contentType
         render (PebbleRenderer.render (template, locale, context))
     }
 
     fun template (template: String, context: Map<String, *> = mapOf<String, Any> ()) {
         template (template, Locale.getDefault(), context)
     }
+
+    fun send (body: String = "", contentType: String = "text/plain", status: Int = 200) {
+        response.status (status)
+        if (body == "")
+            response.send()
+        else
+            response.send(body, contentType)
+    }
+
+    fun ok (body: String = "", contentType: String = "text/plain", status: Int = 200) =
+        send (body, contentType, status)
+
+    fun halt (body: String = "", contentType: String = "text/plain", status: Int = 500) =
+        send (body, contentType, status)
 }
