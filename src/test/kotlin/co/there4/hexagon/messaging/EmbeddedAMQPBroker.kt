038243824d6230e655ff434b6b8f0c93628c44c9
jamming <juanjoaguililla@gmail.com>
2016-05-03 18:39:27 +0200

    Add embedded RabbitMQ tests

---
+++ src/test/kotlin/co/there4/hexagon/messaging/EmbeddedAMQPBroker.kt
@@ -0,0 +1,25 @@
+package co.there4.hexagon.messaging
+
+import com.google.common.io.Files
+import org.apache.qpid.server.Broker
+import org.apache.qpid.server.BrokerOptions
+
+/**
+ * Check: https://dzone.com/articles/mocking-rabbitmq-for-integration-tests
+ */
+class EmbeddedAMQPBroker {
+    private val dir="/home/jam/Projects/jamming/hexagon/src/test/resources"
+    private val BROKER_PORT = 5673
+    private val broker = Broker()
+    private val brokerOptions = BrokerOptions()
+
+    init {
+        brokerOptions.setConfigProperty("qpid.amqp_port", BROKER_PORT.toString())
+        brokerOptions.setConfigProperty("qpid.pass_file", "$dir/passwd.txt")
+        brokerOptions.setConfigProperty("qpid.work_dir", Files.createTempDir().getAbsolutePath())
+        brokerOptions.setInitialConfigurationLocation("$dir/qpid.json")
+    }
+
+    fun startup() { broker.startup(brokerOptions) }
+    fun shutdown() { broker.shutdown() }
+}
---
+++ src/test/kotlin/co/there4/hexagon/messaging/RabbitConnectionTest.kt
@@ -0,0 +1,70 @@
+package co.there4.hexagon.messaging
+
+import org.testng.annotations.AfterClass
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+import java.lang.System.currentTimeMillis
+import kotlin.test.assertFailsWith
+
+@Test class RabbitConnectionTest {
+    private val broker = EmbeddedAMQPBroker()
+
+    private val URI = "amqp://guest:guest@localhost:5673"
+    private val QUEUE = "test"
+    private val QUEUE_ERROR = "error"
+    private val SUFFIX = "DONE"
+    private val DELAY = 10L
+
+    private var consumer: RabbitClient? = null
+    private var client: RabbitClient? = null
+
+    @BeforeClass fun startConsumer () {
+        broker.startup()
+
+        consumer = RabbitClient (URI)
+        consumer?.declareQueue (QUEUE)
+        consumer?.consume (QUEUE, String::class) { a ->
+            Thread.sleep (DELAY)
+            a + SUFFIX
+        }
+
+        consumer?.declareQueue (QUEUE_ERROR)
+        consumer?.consume (QUEUE_ERROR, String::class) { a ->
+            throw RuntimeException("Error with: $a")
+        }
+
+        client = RabbitClient (URI)
+    }
+
+    @AfterClass fun deleteTestQueue () {
+        consumer?.deleteQueue (QUEUE)
+        consumer?.deleteQueue (QUEUE_ERROR)
+        assertFailsWith<IllegalStateException> {
+            consumer?.close()
+        }
+
+        broker.shutdown()
+    }
+
+    fun call_return_expected_results () {
+        val ts = currentTimeMillis ().toString ()
+        assert (client?.call (QUEUE, ts).equals (ts + SUFFIX))
+        val result = client?.call (QUEUE_ERROR, ts) ?: ""
+        assert (result.contains (ts.toString()) && result.contains ("Error with: $ts"))
+
+        broker.shutdown()
+        try {
+            client?.call (QUEUE_ERROR, ts)
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        startConsumer()
+        val ts2 = currentTimeMillis ().toString ()
+        assert (client?.call (QUEUE, ts2).equals (ts2 + SUFFIX))
+        val result2 = client?.call (QUEUE_ERROR, ts2) ?: ""
+        assert (result2.contains (ts2.toString()) && result2.contains ("Error with: $ts2"))
+    }
+
+}
