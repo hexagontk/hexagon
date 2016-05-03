5892efff9cad18dc16dd270e65d364e1f8e85bb3
jamming <juanjoaguililla@gmail.com>
2016-05-03 18:57:22 +0200

    Fix build problem

--- src/test/kotlin/co/there4/hexagon/messaging/EmbeddedAMQPBroker.kt
+++ src/test/kotlin/co/there4/hexagon/messaging/EmbeddedAMQPBroker.kt
@@ -3,12 +3,13 @@ package co.there4.hexagon.messaging
 import com.google.common.io.Files
 import org.apache.qpid.server.Broker
 import org.apache.qpid.server.BrokerOptions
+import java.io.File
 
 /**
  * Check: https://dzone.com/articles/mocking-rabbitmq-for-integration-tests
  */
 class EmbeddedAMQPBroker {
-    private val dir="/home/jam/Projects/jamming/hexagon/src/test/resources"
+    private val dir = File(".").absolutePath + "/src/test/resources"
     private val BROKER_PORT = 5673
     private val broker = Broker()
     private val brokerOptions = BrokerOptions()
