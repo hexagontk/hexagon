a3b4e383b2a9119b93fe288406831e63935b11d1
jamming <juanjoaguililla@gmail.com>
2016-05-05 12:48:23 +0200

    Initial configuration test

---
+++ src/test/kotlin/co/there4/hexagon/configuration/ConfigManagerTest.kt
@@ -0,0 +1,9 @@
+package co.there4.hexagon.configuration
+
+import org.testng.annotations.Test
+
+@Test class ConfigManagerTest {
+    fun get_simple_configuration() {
+        ConfigManager
+    }
+}
