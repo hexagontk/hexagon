20f4f0f12414daaa90bb78dafd53baeb36c4863b
jamming <juanjoaguililla@gmail.com>
2016-05-07 15:34:40 +0200

    Clean up events tests

--- src/test/kotlin/co/there4/hexagon/events/EventsDemo.kt
+++ src/test/kotlin/co/there4/hexagon/events/EventsDemo.kt
@@ -8,28 +8,31 @@ import java.lang.System.nanoTime
 import java.lang.Thread.`yield` as threadYield
 
 @Test class EventsDemo {
-    class TickEvent (val nanos: Long) : Event ("clock.tick")
+    class TickEvent (val nanos: Long) : Event (TickEvent::class.java.name)
 
     companion object : CompanionLogger(EventsDemo::class)
 
     private var tick: Long = 0
 
     @BeforeClass fun startConsumer() {
-        EventManager.on(TickEvent::class, "clock.tick") {
+        EventManager.on(TickEvent::class) {
             info("Tick: ${it.nanos}")
             tick = it.nanos
         }
     }
 
     @AfterClass fun deleteTestQueue() {
-        EventManager.client.deleteQueue("clock.tick")
+        EventManager.client.deleteQueue(TickEvent::class.java.name)
     }
 
     fun events_are_published_properly() {
         val nanos = nanoTime()
         EventManager.publish(TickEvent(nanos))
+
+        // Wait for the consumer to handle the event
         while (tick == 0L)
             threadYield()
+
         assert(tick == nanos)
     }
 }
