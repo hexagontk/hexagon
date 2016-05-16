8f47d2001c628d8825170c4eeff11fa7bdfac0c6
jamming <juanjoaguililla@gmail.com>
2016-05-16 21:27:31 +0200

    Add environment

--- src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
+++ src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
@@ -3,7 +3,7 @@ package co.there4.hexagon.configuration
 import java.lang.System.getenv
 
 object ConfigManager {
-    val environment: String = getenv("ENVIRONMENT")
+//    val environment: String = getenv("ENVIRONMENT") ?: "PROD"
 
     // TODO Get application name from main class
     // TODO Load configuration depending on environment, ie: $ENVIRONMENT.json
