54fb042920b38061ea480366bc28857eb51671f0
jamming <juanjoaguililla@gmail.com>
2016-05-17 13:28:16 +0200

    Add basedir processing in configuration

--- src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
+++ src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
@@ -1,9 +1,11 @@
 package co.there4.hexagon.configuration
 
+import java.io.File
 import java.lang.System.getenv
 
 object ConfigManager {
 //    val environment: String = getenv("ENVIRONMENT") ?: "PROD"
+//    val BASE_DIR = File (ConfigManager::class.java.protectionDomain.codeSource.location.toURI())
 
     // TODO Get application name from main class
     // TODO Load configuration depending on environment, ie: $ENVIRONMENT.json
