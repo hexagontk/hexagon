897aa428339e059f190119f9e6a7905140a612a4
jamming <juanjoaguililla@gmail.com>
2016-05-18 16:40:27 +0200

    Distribution generation changes

--- src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
+++ src/main/kotlin/co/there4/hexagon/configuration/ConfigManager.kt
@@ -1,13 +1,9 @@
 package co.there4.hexagon.configuration
 
-import ch.qos.logback.classic.LoggerContext
-import ch.qos.logback.classic.util.ContextInitializer
 import co.there4.hexagon.util.camelToSnake
-import org.slf4j.LoggerFactory.getILoggerFactory
 import java.io.File
 import java.lang.ClassLoader.getSystemClassLoader
 import java.lang.System.getenv
-import java.lang.System.setProperty
 import java.lang.Thread.currentThread
 import java.util.*
 
@@ -53,17 +49,6 @@ object ConfigManager {
                 .map { it.key as String to it.value }
                 .toMap()
         }
-    }
-
-    fun setupLogging () {
-        setProperty("serviceDir", serviceDir)
-        setProperty("servicePackage", servicePackage)
-        setProperty("serviceName", serviceName)
-
-        val loggerContext = getILoggerFactory() as LoggerContext
-        loggerContext.reset()
-        val contextInitializer = ContextInitializer(loggerContext)
-        contextInitializer.autoConfig()
     }
 
     operator fun get (key: String) = parameters[key]
