1d810ba11261d91bf372698e8b3643de24e13a70
jamming <juanjoaguililla@gmail.com>
2016-08-30 16:08:49 +0200

    PoC changes

--- src/test/kotlin/co/there4/hexagon/demo/DependencyInjectionTest.kt
+++ src/test/kotlin/co/there4/hexagon/demo/DependencyInjectionTest.kt
@@ -1,44 +1,63 @@
 import org.testng.annotations.Test
 import kotlin.reflect.KClass
 
+/*
+ * Example interfaces and implementations
+ */
 interface Foo
-class Foo1 : Foo
-class Foo2 : Foo
-object Foo3 : Foo
+
+class SubFoo1 : Foo
+class SubFoo2 : Foo
+object SubFoo3 : Foo
 
 interface Bar { val foo: Foo }
-class Bar1(override val foo: Foo) : Bar
 
-var registry : Map<KClass<*>, () -> Any> = mapOf()
+class SubBar1(override val foo: Foo) : Bar
+class SubBar2(override val foo: Foo = (Foo::class)()) : Bar
+
+/*
+ * Generators registry and utilities
+ */
+private var registry : Map<KClass<*>, () -> Any> = mapOf()
 
 fun <T : Any> register(type: KClass<T>, provider: () -> T) {
     registry += type to provider
 }
 
 @Suppress("UNCHECKED_CAST")
-operator fun <T : Any> KClass<T>.invoke (): T = registry[this]?.invoke() as T
+operator fun <T : Any> KClass<T>.invoke (): T =
+    registry[this]?.invoke() as? T ?: error("${this.java.name} generator not found")
 
+/*
+ * Tests
+ */
 @Test class DiPoC {
     fun di_just_works() {
-        register (Foo::class, ::Foo1)
+        register (Foo::class, ::SubFoo1)
 
         val foo1 = (Foo::class)()
-        assert(foo1.javaClass == Foo1::class.java)
+        assert(foo1.javaClass == SubFoo1::class.java)
 
-        register (Foo::class, ::Foo2)
+        register (Foo::class, ::SubFoo2)
 
         val foo2 = (Foo::class)()
-        assert(foo2.javaClass == Foo2::class.java)
+        assert(foo2.javaClass == SubFoo2::class.java)
 
-        register (Foo::class) { Foo3 }
+        register (Foo::class) { SubFoo3 }
 
         val foo3 = (Foo::class)()
-        assert(foo3.javaClass == Foo3::class.java)
+        assert(foo3.javaClass == SubFoo3::class.java)
 
-        register (Bar::class) { Bar1((Foo::class)()) }
+        register (Bar::class) { SubBar1((Foo::class)()) }
 
         val bar1 = (Bar::class)()
-        assert(bar1.javaClass == Bar1::class.java)
-        assert(bar1.foo.javaClass == Foo3::class.java)
+        assert(bar1.javaClass == SubBar1::class.java)
+        assert(bar1.foo.javaClass == SubFoo3::class.java)
+
+        register (Bar::class) { SubBar2() }
+
+        val bar2 = (Bar::class)()
+        assert(bar2.javaClass == SubBar2::class.java)
+        assert(bar2.foo.javaClass == SubFoo3::class.java)
     }
 }
