889789a018258a78346c29f444fa52ff38acec32
jamming <juanjoaguililla@gmail.com>
2016-09-05 18:32:15 +0200

    Update "bug?" test

--- src/test/kotlin/co/there4/hexagon/demo/CallbackReferenceIssue.kt
+++ src/test/kotlin/co/there4/hexagon/demo/CallbackReferenceIssue.kt
@@ -1,11 +1,15 @@
 package co.there4.hexagon.demo
 
-//fun String.doSomething() {}
-fun doSomething(receiver: String) {}
+/*
+ * The following two declarations have the same JVM signature, and yet only the second can be
+ * referenced with '::' No matter what the receiver function signature is.
+ */
+//fun String.aCallback() {}
+fun aCallback(receiver: String) {}
 
 fun takeCallback (callback: String.() -> Unit) {}
 //fun takeCallback (callback: (String) -> Unit) {}
 
 fun test() {
-    takeCallback (::doSomething)
+    takeCallback (::aCallback)
 }
