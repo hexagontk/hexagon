3f46ebf719e81d525b9c0a38d8d9b66e86d7b752
jamming <juanjoaguililla@gmail.com>
2016-10-28 12:53:53 +0200

    Powermock PoC

--- src/test/kotlin/co/there4/hexagon/settings/SettingsManagerTest.kt
+++ src/test/kotlin/co/there4/hexagon/settings/SettingsManagerTest.kt
@@ -1,12 +1,18 @@
 package co.there4.hexagon.settings
 
 import co.there4.hexagon.settings.SettingsManager.setting
+import org.testng.annotations.ObjectFactory
 import org.testng.annotations.Test
 
 /**
  * Check `gradle.build` to see the related files creation.
  */
 @Test class SettingsManagerTest {
+    @ObjectFactory fun getObjectFactory() = org.powermock.modules.testng.PowerMockObjectFactory()
+
+    fun power_mock() {
+    }
+
     fun get_configuration_properties() {
         assert(SettingsManager["property"] == "changed")
         assert(setting<String>("property") == "changed")
