ff53340f6e2b598c9d666c8ad0257e7c483ebd82
jamming <juanjoaguililla@gmail.com>
2016-12-17 16:09:18 +0100

    Change Benchmark testing and deployment

--- systems/vagrant.rb
+++ systems/vagrant.rb
@@ -1,7 +1,14 @@
 
-# def upload(config, src, dest)
-#   config.vm.provision :file, source: src, destination: dest
-# end
+$project_name = 'benchmark'
+$resin_url = 'http://www.caucho.com/download/rpm-6.8'
+$resin_version = '4.0.49'
+
+def installDbs(config)
+  config.vm.provision :docker, images: %w(mongo mysql) do |d|
+    d.run 'mongo', image: 'mongo', args: '-p 27017:27017'
+    d.run 'mysql', image: 'mysql', args: '-p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes'
+  end
+end
 
 def upload(config, *paths)
   paths.each do |src, dest| config.vm.provision :file, source: src, destination: dest end
