8434c5426d354b84eb398edca5bcbdb3ff189db3
jamming <juanjoaguililla@gmail.com>
2017-01-14 13:19:24 +0100

    Improve coverage

--- src/main/kotlin/co/there4/hexagon/rest/RestPackage.kt
+++ src/main/kotlin/co/there4/hexagon/rest/RestPackage.kt
@@ -1,5 +1,6 @@
 package co.there4.hexagon.rest
 
+import co.there4.hexagon.repository.FileRepository
 import co.there4.hexagon.repository.MongoIdRepository
 import co.there4.hexagon.repository.MongoRepository
 import co.there4.hexagon.web.Exchange
@@ -23,9 +24,10 @@ fun <T : Any> crud(repository: MongoRepository<T>, readOnly: Boolean = false) {
     server.crud (repository, readOnly)
 }
 
+// TODO Not optimal at all (can be faster using inputStream instead body string)
 fun Server.files(path: String = "/file", fileName: Exchange.() -> String = { request.pathInfo }) {
     get(path) { file(fileName()) }
-    put(path) { }
-    post(path) { }
+//    put(path) { FileRepository.store(fileName(), request.body.byteInputStream(), base64 = true)}
+    post(path) { FileRepository.store(fileName(), request.body.byteInputStream(), base64 = true) }
 //    delete(path) { FileRepository.gridfs.delete(fileName()) }
 }
--- src/test/kotlin/co/there4/hexagon/web/ClientTest.kt
+++ src/test/kotlin/co/there4/hexagon/web/ClientTest.kt
@@ -1,6 +1,9 @@
 package co.there4.hexagon.web
 
+import co.there4.hexagon.rest.files
+import co.there4.hexagon.serialization.serialize
 import co.there4.hexagon.web.servlet.JettyServletServer
+import org.asynchttpclient.Response
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
@@ -12,13 +15,21 @@ import org.testng.annotations.Test
     }
 
     @BeforeClass fun startup() {
+        server.files()
+
         server.post {
             response.contentType = "application/json; charset=utf-8"
             ok(request.body)
         }
-        server.get {
-            ok(request.body)
-        }
+
+        server.get { ok(request.body) }
+        server.head { ok(request.body) }
+        server.put { ok(request.body) }
+        server.delete { ok(request.body) }
+        server.trace { ok(request.body) }
+        server.options { ok(request.body) }
+        server.patch { ok(request.body) }
+
         server.run()
     }
 
@@ -38,5 +49,24 @@ import org.testng.annotations.Test
 
         client.get("/", "application/json") { "foo" }
         client.get("/") { "foo" }
+    }
+
+    fun http_methods_with_objects_work_ok() {
+        val parameter = mapOf("key" to "value")
+        checkResponse(client.get("/") { parameter }, parameter)
+        checkResponse(client.head("/") { parameter }, null)
+        checkResponse(client.post("/") { parameter }, parameter)
+        checkResponse(client.put("/") { parameter }, parameter)
+        checkResponse(client.delete("/") { parameter }, parameter)
+        checkResponse(client.trace("/") { parameter }, parameter)
+        checkResponse(client.options("/") { parameter }, parameter)
+        checkResponse(client.patch("/") { parameter }, parameter)
+    }
+
+    fun files_can_be_fetched_and_downloaded() {}
+
+    private fun checkResponse(response: Response, parameter: Map<String, String>?) {
+        assert(response.statusCode == 200)
+        assert(response.responseBody.trim() == parameter?.serialize()?.trim() ?: "")
     }
 }
