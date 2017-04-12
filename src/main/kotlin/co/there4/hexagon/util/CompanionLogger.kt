22b32141d165b69f73724638ba566f99a140aaf2
jamming <juanjoaguililla@gmail.com>
2017-04-13 00:48:22 +0200

    Ease logging usage
    (cherry picked from commit 3f2d35a)

--- src/main/kotlin/co/there4/hexagon/util/CompanionLogger.kt
+++ src/main/kotlin/co/there4/hexagon/util/CompanionLogger.kt
@@ -8,27 +8,14 @@ import kotlin.reflect.KClass
 open class CompanionLogger(clazz: KClass<out Any>){
     private val logger = getLogger(clazz.java)
 
-    fun trace (message: String, arguments: Map<String, *> = mapOf<String, Any>()) =
-        wrapLog { logger.trace(message, arguments) }
-
-    fun debug (message: String, arguments: Map<String, *> = mapOf<String, Any>()) =
-        wrapLog { logger.debug(message, arguments) }
-
-    fun info (message: String, arguments: Map<String, *> = mapOf<String, Any>()) =
-        wrapLog { logger.info(message, arguments) }
-
-    fun warn (message: String, arguments: Map<String, *> = mapOf<String, Any>()) =
-        wrapLog { logger.warn(message, arguments) }
-
-    fun err(message: String, arguments: Map<String, *> = mapOf<String, Any>()) =
-        wrapLog { logger.error(message, arguments) }
-
-    fun warn (message: String, exception: Throwable) = wrapLog { logger.warn(message, exception) }
-
-    fun err(message: String, exception: Throwable) = wrapLog { logger.error(message, exception) }
-
-    fun flare (message: String = "") = wrapLog { logger.trace("$flarePrefix $message") }
-
+    fun trace (message: String, vararg arguments: Any) = wrap { logger.trace(message, arguments) }
+    fun debug (message: String, vararg arguments: Any) = wrap { logger.debug(message, arguments) }
+    fun info (message: String, vararg arguments: Any) = wrap { logger.info(message, arguments) }
+    fun warn (message: String, vararg arguments: Any) = wrap { logger.warn(message, arguments) }
+    fun err(message: String, vararg arguments: Any) = wrap { logger.error(message, arguments) }
+    fun warn (message: String, exception: Throwable) = wrap { logger.warn(message, exception) }
+    fun err(message: String, exception: Throwable) = wrap { logger.error(message, exception) }
+    fun flare (message: String = "") = wrap { logger.trace("$flarePrefix $message") }
     fun time (startNanos: Long, message: String?) = printNanos(message) { nanoTime() - startNanos }
 
     fun <T> time (message: String? = null, block: () -> T): T {
@@ -38,12 +25,11 @@ open class CompanionLogger(clazz: KClass<out Any>){
         return result
     }
 
-    private fun printNanos(message: String? = null, lambda: () -> Long): Long = lambda().let {
-        logger.trace((if (message != null) message + " : " else "TIME : ") + formatNanos(it))
-        it
+    private fun printNanos(message: String? = null, lambda: () -> Long): Long = lambda().also {
+        logger.trace((if (message != null) "$message : " else "TIME : ") + formatNanos(it))
     }
 
-    private fun wrapLog(log: () -> Unit) {
+    private fun wrap(log: () -> Unit) {
         MDC.put("jvmId", jvmId)
         MDC.put("hostname", hostname)
         MDC.put("ip", ip)
