e430618384c40811aaf86605c401d78063a3583d
jamming <juanjoaguililla@gmail.com>
2017-05-30 08:00:43 +0200

    Update project templates

--- hexagon_templates/hexagon-service/lazybones.groovy
+++ hexagon_templates/hexagon-service/lazybones.groovy
@@ -13,21 +13,25 @@ String className = transformText (projectName.tr ('_', '-'), from: HYPHENATED, t
 String group = askParameter ('group', 'org.example')
 String version = askParameter ('version', '0.1')
 String description = askParameter ('description', "Service's description")
+String bindPort = askParameter ('bindPort', "Service's port")
 
 Map<Object, Object> props = [
     group : group,
     version : version,
     description : description,
     projectName : projectName,
-    className : className
+    className : className,
+    bindPort : bindPort
 ]
 
 processTemplates 'readme.md', props
 processTemplates 'build.gradle', props
 processTemplates 'settings.gradle', props
 processTemplates 'gradle.properties', props
+processTemplates 'dockerfile', props
 processTemplates 'src/main/kotlin/Service.kt', props
 processTemplates 'src/main/resources/logback.xml', props
+processTemplates 'src/main/resources/service.yaml', props
 processTemplates 'src/test/kotlin/ServiceTest.kt', props
 processTemplates 'src/test/resources/logback-test.xml', props
 
