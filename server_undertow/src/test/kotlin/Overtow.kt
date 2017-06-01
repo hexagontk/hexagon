5a970e73ca4d667e3dad1d861fc902248a8ce6a2
jamming <juanjoaguililla@gmail.com>
2017-06-01 18:08:40 +0200

    Example

---
+++ server_undertow/src/test/kotlin/Overtow.kt
@@ -0,0 +1,28 @@
+
+import io.undertow.Undertow
+import io.undertow.server.RoutingHandler
+import io.undertow.server.handlers.PathHandler
+import io.undertow.util.Headers.CONTENT_TYPE
+
+fun main(vararg args: String) {
+    val hnd = PathHandler().addExactPath("/foo") {
+        it.responseSender.send("f")
+    }
+
+    hnd.addExactPath(
+        "/bar",
+        RoutingHandler().get("/b/:id") {
+            it.responseSender.send(it.pathParameters["id"]?.first() ?: "404")
+        }
+    )
+
+    val server = Undertow.builder()
+        .addHttpListener(7070, "localhost")
+        .setHandler(hnd)
+//        .setHandler {
+//            it.responseHeaders.put(CONTENT_TYPE, "text/plain")
+//            it.responseSender.send("Hello World")
+//        }
+        .build()
+    server.start()
+}
