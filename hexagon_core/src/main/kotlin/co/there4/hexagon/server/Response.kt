8881d8c0d19fb5c1aeac9869c4f119cd29cee924
jamming <juanjoaguililla@gmail.com>
2017-06-24 01:23:55 +0200

    Check response status changes

--- hexagon_core/src/main/kotlin/co/there4/hexagon/server/Response.kt
+++ hexagon_core/src/main/kotlin/co/there4/hexagon/server/Response.kt
@@ -18,11 +18,16 @@ class Response (private val response: EngineResponse) {
 
     var status: Int
         get() = response.status
-        set(value) { response.status = value }
+        set(value) {
+            response.status = value
+            statusChanged = true
+        }
 
     var contentType: String?
         get() = response.contentType
         set(value) { response.contentType = value }
+
+    var statusChanged: Boolean = false
 
     fun getMimeType (file: String): String? = response.getMimeType(file)
     fun addHeader (name: String, value: String) = response.addHeader(name, value)
