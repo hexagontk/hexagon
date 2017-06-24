ec3995aaf1559de600eda06771e1c01360b9c0f8
jamming <juanjoaguililla@gmail.com>
2017-06-24 11:05:10 +0200

    Upgrade Cron Utils to latest version

--- hexagon_core/src/main/kotlin/co/there4/hexagon/scheduler/CronScheduler.kt
+++ hexagon_core/src/main/kotlin/co/there4/hexagon/scheduler/CronScheduler.kt
@@ -1,12 +1,13 @@
 package co.there4.hexagon.scheduler
 
 import co.there4.hexagon.helpers.CachedLogger
+import co.there4.hexagon.helpers.error
 import com.cronutils.model.CronType.QUARTZ
 import com.cronutils.model.definition.CronDefinitionBuilder.instanceDefinitionFor as cronDefinition
 import com.cronutils.model.time.ExecutionTime
 import com.cronutils.parser.CronParser
+import org.threeten.bp.ZonedDateTime
 import java.lang.Runtime.getRuntime
-import java.time.ZonedDateTime
 import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.TimeUnit.SECONDS
 
@@ -19,13 +20,14 @@ class CronScheduler(threads: Int = getRuntime().availableProcessors()) {
     fun schedule (cronExpression: String, callback: () -> Unit) {
         val cron = cronParser.parse (cronExpression)
         val cronExecution = ExecutionTime.forCron(cron)
-        val nextExecution = cronExecution.timeToNextExecution(ZonedDateTime.now ())
-        val nextExecutionSeconds = nextExecution.seconds
 
-        scheduler.schedule ({ function (callback, cronExecution) }, nextExecutionSeconds, SECONDS)
+        scheduler.schedule ({ function (callback, cronExecution) }, delay(cronExecution), SECONDS)
     }
 
     fun shutdown () { scheduler.shutdown() }
+
+    private fun delay(cronExecution: ExecutionTime) =
+        cronExecution.timeToNextExecution(ZonedDateTime.now ()).orNull()?.seconds ?: error
 
     private fun function(callback: () -> Unit, cronExecution: ExecutionTime) {
         try {
@@ -35,9 +37,6 @@ class CronScheduler(threads: Int = getRuntime().availableProcessors()) {
             error("Error executing cron job", e)
         }
 
-        val nextExecution = cronExecution.timeToNextExecution(ZonedDateTime.now ())
-        val nextExecutionSeconds = nextExecution.seconds
-
-        scheduler.schedule ({ function (callback, cronExecution) }, nextExecutionSeconds, SECONDS)
+        scheduler.schedule ({ function (callback, cronExecution) }, delay(cronExecution), SECONDS)
     }
 }
