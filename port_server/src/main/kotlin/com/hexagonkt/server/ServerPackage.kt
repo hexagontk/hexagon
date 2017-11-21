d7677281e269366512a5346d4b8671026ea53011
jamming <juanjoaguililla@gmail.com>
2017-11-21 23:34:10 +0100

    Add return types in functions

--- port_server/src/main/kotlin/com/hexagonkt/server/ServerPackage.kt
+++ port_server/src/main/kotlin/com/hexagonkt/server/ServerPackage.kt
@@ -58,23 +58,23 @@ fun path(path: String = "/") = Path(path)
 /** Shortcut to create a route for a filter (with all methods). */
 fun all(path: String = "/"): Route = Route(Path(path), ALL)
 /** Shortcut to create a GET route. */
-fun get(path: String = "/") = Route(Path(path), GET)
+fun get(path: String = "/"): Route = Route(Path(path), GET)
 /** Shortcut to create a HEAD route. */
-fun head(path: String = "/") = Route(Path(path), HEAD)
+fun head(path: String = "/"): Route = Route(Path(path), HEAD)
 /** Shortcut to create a POST route. */
-fun post(path: String = "/") = Route(Path(path), POST)
+fun post(path: String = "/"): Route = Route(Path(path), POST)
 /** Shortcut to create a PUT route. */
-fun put(path: String = "/") = Route(Path(path), PUT)
+fun put(path: String = "/"): Route = Route(Path(path), PUT)
 /** Shortcut to create a DELETE route. */
-fun delete(path: String = "/") = Route(Path(path), DELETE)
+fun delete(path: String = "/"): Route = Route(Path(path), DELETE)
 /** Shortcut to create a TRACE route. */
-fun tracer(path: String = "/") = Route(Path(path), TRACE)
+fun tracer(path: String = "/"): Route = Route(Path(path), TRACE)
 /** Shortcut to create a OPTIONS route. */
-fun options(path: String = "/") = Route(Path(path), OPTIONS)
+fun options(path: String = "/"): Route = Route(Path(path), OPTIONS)
 /** Shortcut to create a PATCH route. */
-fun patch(path: String = "/") = Route(Path(path), PATCH)
+fun patch(path: String = "/"): Route = Route(Path(path), PATCH)
 
 /** Shortcut to create a route from a method and a path. */
-infix fun HttpMethod.at(path: String) = Route(Path(path), this)
+infix fun HttpMethod.at(path: String): Route = Route(Path(path), this)
 /** Shortcut to create a route from a method and a path. */
-infix fun LinkedHashSet<HttpMethod>.at(path: String) = Route(Path(path), this)
+infix fun LinkedHashSet<HttpMethod>.at(path: String): Route = Route(Path(path), this)
