88041d9df1bf9f979eba227c40c655d096bb3c6e
jamming <juanjoaguililla@gmail.com>
2017-11-29 22:46:41 +0100

    Add tests

--- hexagon_examples/src/test/kotlin/com/hexagonkt/examples/ExamplesTest.kt
+++ hexagon_examples/src/test/kotlin/com/hexagonkt/examples/ExamplesTest.kt
@@ -1,6 +1,23 @@
 package com.hexagonkt.examples
 
 import org.testng.annotations.Test
+import java.net.URL
 
 
-@Test abstract class ExamplesTest
+@Test class ExamplesTest {
+    fun `Si uso 'to' creo un 'par'` () {
+        val pair = "a" to 1
+        @Suppress("USELESS_IS_CHECK") assert(pair is Pair<String, Int>)
+        assert(pair.first == "a") // == es como equals
+        assert(pair.first === "a") // === es como ==
+        val u1 = URL("http://example.com")
+        val u2 = URL("http://example.com")
+        assert(u1 == u2)
+        assert(u1 !== u2)
+    }
+}
+
+@Test class Ex2 {
+    fun foo() {
+    }
+}
