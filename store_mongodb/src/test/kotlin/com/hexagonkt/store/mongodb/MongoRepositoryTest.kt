73282579d9b0289aa94d4cf08d87f778b993b60f
jamming <juanjoaguililla@gmail.com>
2017-12-16 12:09:11 +0100

    Create MongoDB adapter (from store port)

---
+++ store_mongodb/src/test/kotlin/com/hexagonkt/store/mongodb/MongoRepositoryTest.kt
@@ -0,0 +1,25 @@
+package com.hexagonkt.store.mongodb
+
+import org.testng.annotations.Test
+import java.io.File
+
+@Test class MongoRepositoryTest {
+    val repository = mongoIdRepository(Company::id)
+
+    // TODO Check inserted data
+    fun resources_are_loaded() {
+        repository.loadData("companies.json")
+        repository.delete()
+        repository.importResource("companies.json")
+        repository.delete()
+
+        // File paths change from IDE to build tool
+        val file = File("hexagon_core/src/test/resources/data/companies.json").let {
+            if (it.exists()) it
+            else File("src/test/resources/companies.json")
+        }
+
+        repository.importFile(file)
+        repository.delete()
+    }
+}
