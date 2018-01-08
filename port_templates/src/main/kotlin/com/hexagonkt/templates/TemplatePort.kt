ece57d57d4756d43474e67de6de4f399e14aa76d
jaguililla <juanjoaguililla@gmail.com>
2018-01-08 21:05:54 +0100

    Add utility method

--- port_templates/src/main/kotlin/com/hexagonkt/templates/TemplatePort.kt
+++ port_templates/src/main/kotlin/com/hexagonkt/templates/TemplatePort.kt
@@ -4,4 +4,7 @@ import java.util.*
 
 interface TemplatePort {
     fun render (resource: String, locale: Locale, context: Map<String, *>): String
+
+    fun render (resource: String, locale: Locale, vararg context: Pair<String, *>): String =
+        render (resource, locale, linkedMapOf(*context))
 }
