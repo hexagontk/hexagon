d55c52de8fe24598c6f8932b7b1c71a9ad864fb3
jamming <juanjoaguililla@gmail.com>
2018-01-08 07:18:17 +0100

    Add comment

--- hexagon_core/src/main/kotlin/com/hexagonkt/HttpMethod.kt
+++ hexagon_core/src/main/kotlin/com/hexagonkt/HttpMethod.kt
@@ -2,6 +2,9 @@ package com.hexagonkt
 
 /**
  * Enum for the supported HTTP methods.
+ * TODO move to `http` subpackage
+ * TODO move to `port_http`
+ * TODO move `port_server` to `port_http_server` and `port_client` to `port_http_client`
  */
 enum class HttpMethod {
     /** HTTP GET method. */
