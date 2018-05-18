014e7e4f5c181965f0916e2f4939feb8804a7e99
jamming <juanjoaguililla@gmail.com>
2018-05-19 01:24:59 +0200

    Improve coverage

--- hexagon_vertx/src/test/kotlin/com/hexagonkt/vertx/store/StoreTest.kt
+++ hexagon_vertx/src/test/kotlin/com/hexagonkt/vertx/store/StoreTest.kt
@@ -1,6 +1,9 @@
 package com.hexagonkt.vertx.store;
 
+import com.hexagonkt.sync
+import com.hexagonkt.vertx.createVertx
 import io.vertx.core.Vertx
+import io.vertx.kotlin.coroutines.await
 import org.junit.Test
 
 abstract class StoreTest<T : Any, K : Any> {
@@ -10,7 +13,18 @@ abstract class StoreTest<T : Any, K : Any> {
     abstract fun modifyEntity(entity: T): T
     open fun createTestEntities(): List<T> = (0..9).map { createEntity(it) }
 
-    @Test fun `Entities are stored`() {
+    @Test fun `Entities are stored`() = sync {
+        val testEntities = createTestEntities()
+        val store = store(createVertx())
+        store.drop()
 
+        store.saveMany(testEntities).await()
+
+        testEntities.forEach {
+            store.saveOne(it).await()
+        }
+
+        // TODO Fix this!
+//        store.saveMany(testEntities).await()
     }
 }
