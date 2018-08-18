b55745fa5690761dd963927187ebc166695f34fb
jamming <juanjoaguililla@gmail.com>
2018-08-18 12:32:15 +0200

    Move stress tests to Gatling

---
+++ hexagon_benchmark/src/test/kotlin/com/hexagonkt/HexagonSimulation.scala
@@ -0,0 +1,24 @@
+package com.hexagonkt
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class HexagonSimulation extends Simulation {
+  val httpConf = http
+    .baseURL ("http://computer-database.gatling.io")
+    .acceptHeader ("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader ("1")
+    .acceptLanguageHeader ("en-US,en;q=0.5")
+    .acceptEncodingHeader ("gzip, deflate")
+    .userAgentHeader ("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario ("BasicSimulation")
+    .exec (
+      http ("request_1")
+        .get ("/"))
+    .pause (5)
+
+  setUp (
+    scn.inject (atOnceUsers (1))
+  ).protocols (httpConf)
+}
