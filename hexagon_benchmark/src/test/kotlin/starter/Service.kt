54dd6bfa3769dc11f01fef42cc45369a8264564a
jamming <juanjoaguililla@gmail.com>
2018-08-22 00:07:25 +0200

    Update Lazybones template

--- hexagon_benchmark/src/test/kotlin/starter/Service.kt
+++ hexagon_benchmark/src/test/kotlin/starter/Service.kt
@@ -1,16 +1,12 @@
 package starter
 
 import java.time.LocalDateTime.now
-import javax.servlet.annotation.WebListener
 
 import com.hexagonkt.server.*
 import com.hexagonkt.server.jetty.JettyServletAdapter
-import com.hexagonkt.server.servlet.ServletServer
+import com.hexagonkt.settings.SettingsManager
 
-/**
- * Routes setup. It is in its own method to be able to call it for the Webapp and from the Service.
- */
-val router = router {
+val server: Server = server(JettyServletAdapter(), SettingsManager.settings) {
     before {
         response.addHeader("Server", "Servlet/3.1")
         response.addHeader("Transfer-Encoding", "chunked")
@@ -21,16 +17,9 @@ val router = router {
 }
 
 /**
- * Main Webapp class.
- */
-@Suppress("unused")
-@WebListener class Web : ServletServer (router)
-
-val server = Server(serverEngine = JettyServletAdapter(), router = router)
-
-/**
  * Start the service from the command line.
  */
 fun main(vararg args: String) {
     server.run()
 }
+
--- hexagon_starters/hexagon-service/src/main/kotlin/Service.kt
+++ hexagon_starters/hexagon-service/src/main/kotlin/Service.kt
@@ -1,16 +1,12 @@
 package ${group}
 
 import java.time.LocalDateTime.now
-import javax.servlet.annotation.WebListener
 
 import com.hexagonkt.server.*
 import com.hexagonkt.server.jetty.JettyServletAdapter
-import com.hexagonkt.server.servlet.ServletServer
+import com.hexagonkt.settings.SettingsManager
 
-/**
- * Routes setup. It is in its own method to be able to call it for the Webapp and from the Service.
- */
-val router = router {
+val server: Server = server(JettyServletAdapter(), SettingsManager.settings) {
     before {
         response.addHeader("Server", "Servlet/3.1")
         response.addHeader("Transfer-Encoding", "chunked")
@@ -19,14 +15,6 @@ val router = router {
 
     get("/text") { ok("Hello, World!", "text/plain") }
 }
-
-/**
- * Main Webapp class.
- */
-@Suppress("unused")
-@WebListener class Web : ServletServer (router)
-
-val server = Server(serverEngine = JettyServletAdapter(), router = router)
 
 /**
  * Start the service from the command line.
