d2d9bd01550a5eb97a05c60c3a244815b45a36ef
jamming <juanjoaguililla@gmail.com>
2018-09-18 07:34:41 +0200

    Develop stress tests

--- hexagon_benchmark/src/test/scala/com/hexagonkt/GatlingTest.scala
+++ hexagon_benchmark/src/test/scala/com/hexagonkt/GatlingTest.scala
@@ -2,16 +2,16 @@ package com.hexagonkt
 
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import org.testng.annotations.Test
+import org.testng.annotations.{Ignore, Test}
 
-@Test class GatlingTest {
+@Test @Ignore class GatlingTest {
 
   @Test def runGatlingStressTests () {
 
     val properties = new GatlingPropertiesBuilder
     val buildDir = System.getProperty("buildDir")
 
-    properties.simulationClass (classOf [HexagonSimulation].getName)
+    properties.simulationClass (classOf [BenchmarkSimulation].getName)
     properties.outputDirectoryBaseName ("gatling")
     properties.resultsDirectory(if (buildDir == null) "build" else buildDir)
 
