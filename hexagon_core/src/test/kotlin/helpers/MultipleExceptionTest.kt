f0a610469e1c3baee2e6b5ef45f5ea91a9534178
Juanjo Aguililla <juanjoaguililla@gmail.com>
2018-10-16 22:13:21 +0200

    Fix test

--- hexagon_core/src/test/kotlin/helpers/MultipleExceptionTest.kt
+++ hexagon_core/src/test/kotlin/helpers/MultipleExceptionTest.kt
@@ -7,14 +7,14 @@ import org.testng.annotations.Test
     @Test fun `'MultipleException' contains a list of causes`() {
         val causes = (0..9).map { RuntimeException (it.toString()) }
         val exception = MultipleException("Coded exception", *causes.toTypedArray())
-        val exceptionVararg = MultipleException(*causes.toTypedArray())
 
         assert (exception.causes.size == 10)
         assert (exception.message == "Coded exception")
-        assert (exceptionVararg.causes.size == 10)
-        assert (exceptionVararg.message == "Coded exception")
-
         exception.causes.forEachIndexed { ii, e -> assert(e.message == ii.toString()) }
+
+        val exceptionVararg = MultipleException(*causes.toTypedArray())
+
+        assert (exceptionVararg.causes.size == 10)
         exceptionVararg.causes.forEachIndexed { ii, e -> assert(e.message == ii.toString()) }
     }
 }
