1ca0c3420849aba3a8f0ce5cd03995aa5a6a6c85
Juanjo Aguililla <juanjoaguililla@gmail.com>
2018-12-28 21:12:32 +0100

    Clean web worker

--- hexagon_site/assets/service_worker.js
+++ hexagon_site/assets/service_worker.js
@@ -1,42 +1,44 @@
-var dataCacheName = 'template-pwa';
-var cacheName = 'template-pwa';
+
+var dataCacheName = 'hexagon_pwa';
+var cacheName = dataCacheName;
+
 var filesToCache = [
     '/',
-    "./img",
-    "./img/architecture.svg",
-    "./img/logo.svg",
-    "./img/logo_white.svg",
-    "./img/logo_wide.svg",
-    "./index.html",
-    "./manifest.json",
-    "./service_worker.js",
-    "./favicon.ico",
-    "./tile-large.png",
-    "./tile-medium.png",
-    "./tile-small.png",
-    "./css",
-    "./css/base.css",
-    "./css/index.css",
-    "./css/page.css"
+    "/css",
+    "/css/base.css",
+    "/css/index.css",
+    "/css/page.css",
+    "/img",
+    "/img/architecture.svg",
+    "/img/logo.svg",
+    "/img/logo_white.svg",
+    "/img/logo_wide.svg",
+    "/index.html",
+    "/manifest.json",
+    "/service_worker.js",
+    "/favicon.ico",
+    "/tile-large.png",
+    "/tile-medium.png",
+    "/tile-small.png"
 ];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
+    console.log('Worker Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
+            console.log('Worker Caching app shell');
             return cache.addAll(filesToCache);
         })
     );
 });
 
 self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
+    console.log('Worker Activate');
     e.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
                 if (key !== cacheName && key !== dataCacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
+                    console.log('Worker Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -46,7 +48,7 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-    console.log('[Service Worker] Fetch', e.request.url);
+    console.log('Worker Fetch', e.request.url);
     e.respondWith(
         caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
