ce10ba3a1003aef6c2a2a3c1af202a2c6d6a8961
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-03-08 21:55:15 +0100

    Refactor

--- port_http_server/src/test/kotlin/com/hexagonkt/http/server/examples/SessionTest.kt
+++ port_http_server/src/test/kotlin/com/hexagonkt/http/server/examples/SessionTest.kt
@@ -2,7 +2,6 @@ package com.hexagonkt.http.server.examples
 
 import com.hexagonkt.http.client.Client
 import com.hexagonkt.helpers.Logger
-import com.hexagonkt.http.server.Router
 import com.hexagonkt.http.server.Server
 import com.hexagonkt.http.server.ServerPort
 import org.asynchttpclient.Response
@@ -10,63 +9,64 @@ import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
-@Test abstract  class SessionTest(adapter: ServerPort) {
-    private val log: Logger = Logger(this)
+@Test abstract class SessionTest(adapter: ServerPort) {
+    private val log: Logger = Logger(SessionTest::class)
 
     private val server: Server by lazy {
         Server(adapter) {
-            get("/session/id") {
-                val id: String = session.id ?: "null"
-                assert(id == session.id ?: "null")
-                ok(id)
-            }
+            path("/session") {
+                get("/id") {
+                    val id: String = session.id ?: "null"
+                    log.info { "Session ID: $id" }
+                    ok(id)
+                }
 
-            get("/session/access") { ok(session.lastAccessedTime?.toString() ?: "null") }
+                get("/access") {
+                    ok(session.lastAccessedTime?.toString() ?: "null")
+                }
 
-            get("/session/new") {
-                try {
+                get("/new") {
                     ok(session.isNew())
                 }
-                catch (e: Exception) {
-                    halt("Session error")
+
+                get("/inactive") {
+                    val inactiveInterval = session.maxInactiveInterval ?: "null"
+                    session.maxInactiveInterval = 999
+                    assert(inactiveInterval == session.maxInactiveInterval ?: "null")
+                    ok(inactiveInterval)
                 }
-            }
 
-            get("/session/inactive") {
-                val inactiveInterval = session.maxInactiveInterval ?: "null"
-                session.maxInactiveInterval = 999
-                assert(inactiveInterval == session.maxInactiveInterval ?: "null")
-                ok(inactiveInterval)
-            }
+                get("/creation") { ok(session.creationTime ?: "null") }
 
-            get("/session/creation") { ok(session.creationTime ?: "null") }
+                post("/invalidate") { session.invalidate() }
 
-            post("/session/invalidate") { session.invalidate() }
+                path("/{key}") {
+                    put("/{value}") {
+                        session.setAttribute(pathParameters["key"], pathParameters["value"])
+                    }
 
-            put("/session/{key}/{value}") {
-                session.setAttribute(pathParameters["key"], pathParameters["value"])
-            }
+                    get {
+                        ok(session.getAttribute(pathParameters["key"]).toString())
+                    }
 
-            get("/session/{key}") {
-                ok(session.getAttribute(pathParameters["key"]).toString())
-            }
+                    delete {
+                        session.removeAttribute(pathParameters["key"])
+                    }
+                }
 
-            delete("/session/{key}") {
-                session.removeAttribute(pathParameters["key"])
-            }
+                get {
+                    val attributeTexts = session.attributes.entries.map { it.key + " : " + it.value }
 
-            get("/session") {
-                val attributeTexts = session.attributes.entries.map { it.key + " : " + it.value }
+                    response.setHeader("attributes", attributeTexts.joinToString(", "))
+                    response.setHeader("attribute values", session.attributes.values.joinToString(", "))
+                    response.setHeader("attribute names", session.attributes.keys.joinToString(", "))
 
-                response.setHeader("attributes", attributeTexts.joinToString(", "))
-                response.setHeader("attribute values", session.attributes.values.joinToString(", "))
-                response.setHeader("attribute names", session.attributes.keys.joinToString(", "))
+                    response.setHeader("creation", session.creationTime.toString())
+                    response.setHeader("id", session.id ?: "")
+                    response.setHeader("last access", session.lastAccessedTime.toString())
 
-                response.setHeader("creation", session.creationTime.toString())
-                response.setHeader("id", session.id ?: "")
-                response.setHeader("last access", session.lastAccessedTime.toString())
-
-                response.status = 200
+                    response.status = 200
+                }
             }
         }
     }
