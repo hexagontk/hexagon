2d848695fe4b28daf3b0272f3f2c68504090f2f9
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-03-19 19:07:12 +0100

    Add Todo test and example

--- hexagon_web/src/test/kotlin/com/hexagonkt/web/examples/TodoTest.kt
+++ hexagon_web/src/test/kotlin/com/hexagonkt/web/examples/TodoTest.kt
@@ -5,21 +5,25 @@ import com.hexagonkt.helpers.require
 import com.hexagonkt.http.client.Client
 import com.hexagonkt.http.server.Server
 import com.hexagonkt.http.server.ServerPort
-import org.asynchttpclient.Response
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
+/**
+ * TODO Parse post body with JSON/YAML
+ * TODO Serialize response with accept content type
+ * TODO Use templates
+ */
 @Test abstract class TodoTest(adapter: ServerPort) {
 
     // sample
-    private val log: Logger = Logger(TodoTest::class)
+    val log: Logger = Logger(TodoTest::class)
 
     data class Task(val number: Int, val title: String, val description: String)
 
     val taskList = listOf(Task(1, "Don Quixote", "Miguel de Cervantes"))
 
-    private val tasks: MutableMap<Int, Task> =
+    val tasks: MutableMap<Int, Task> =
         LinkedHashMap(taskList.map { it.number to it }.toMap())
 
     val server: Server by lazy {
@@ -84,53 +88,53 @@ import org.testng.annotations.Test
         server.stop()
     }
 
-    @Test fun createBook() {
-        val result = client.post("/books?author=Vladimir%20Nabokov&title=Lolita")
-        assert(Integer.valueOf(result.responseBody) > 0)
-        assert(201 == result.statusCode)
-    }
-
-    @Test fun listBooks() {
-        val result = client.get("/books")
-        assertResponseContains(result, "100", "101")
-    }
-
-    @Test fun getBook() {
-        val result = client.get("/books/101")
-        assertResponseContains(result, "William Shakespeare", "Hamlet")
-    }
-
-    @Test fun updateBook() {
-        val resultPut = client.put("/books/100?title=Don%20Quixote")
-        assertResponseContains(resultPut, "100", "updated")
-
-        val resultGet = client.get("/books/100")
-        assertResponseContains(resultGet, "Miguel de Cervantes", "Don Quixote")
-    }
-
-    @Test fun deleteBook() {
-        val result = client.delete("/books/102")
-        assertResponseContains(result, "102", "deleted")
-    }
-
-    @Test fun bookNotFound() {
-        val result = client.get("/books/9999")
-        assertResponseContains(result, 404, "not found")
-    }
-
-    @Test fun invalidMethodReturns405() {
-        val result = client.options("/books/9999")
-        assert(405 == result.statusCode)
-    }
-
-    private fun assertResponseContains(response: Response?, status: Int, vararg content: String) {
-        assert(response?.statusCode == status)
-        content.forEach {
-            assert(response?.responseBody?.contains(it) ?: false)
-        }
-    }
-
-    private fun assertResponseContains(response: Response?, vararg content: String) {
-        assertResponseContains(response, 200, *content)
-    }
+//    @Test fun createBook() {
+//        val result = client.post("/books?author=Vladimir%20Nabokov&title=Lolita")
+//        assert(Integer.valueOf(result.responseBody) > 0)
+//        assert(201 == result.statusCode)
+//    }
+//
+//    @Test fun listBooks() {
+//        val result = client.get("/books")
+//        assertResponseContains(result, "100", "101")
+//    }
+//
+//    @Test fun getBook() {
+//        val result = client.get("/books/101")
+//        assertResponseContains(result, "William Shakespeare", "Hamlet")
+//    }
+//
+//    @Test fun updateBook() {
+//        val resultPut = client.put("/books/100?title=Don%20Quixote")
+//        assertResponseContains(resultPut, "100", "updated")
+//
+//        val resultGet = client.get("/books/100")
+//        assertResponseContains(resultGet, "Miguel de Cervantes", "Don Quixote")
+//    }
+//
+//    @Test fun deleteBook() {
+//        val result = client.delete("/books/102")
+//        assertResponseContains(result, "102", "deleted")
+//    }
+//
+//    @Test fun bookNotFound() {
+//        val result = client.get("/books/9999")
+//        assertResponseContains(result, 404, "not found")
+//    }
+//
+//    @Test fun invalidMethodReturns405() {
+//        val result = client.options("/books/9999")
+//        assert(405 == result.statusCode)
+//    }
+//
+//    private fun assertResponseContains(response: Response?, status: Int, vararg content: String) {
+//        assert(response?.statusCode == status)
+//        content.forEach {
+//            assert(response?.responseBody?.contains(it) ?: false)
+//        }
+//    }
+//
+//    private fun assertResponseContains(response: Response?, vararg content: String) {
+//        assertResponseContains(response, 200, *content)
+//    }
 }
