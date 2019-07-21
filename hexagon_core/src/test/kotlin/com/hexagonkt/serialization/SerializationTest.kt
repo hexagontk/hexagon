4c167bb13a7d4cf41d462a084ed6184af4a65f17
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-07-21 21:48:07 +0200

    Improve tests

--- hexagon_core/src/test/kotlin/com/hexagonkt/serialization/SerializationTest.kt
+++ hexagon_core/src/test/kotlin/com/hexagonkt/serialization/SerializationTest.kt
@@ -4,24 +4,26 @@ import com.hexagonkt.serialization.SerializationManager.formats
 import org.testng.annotations.Test
 import kotlin.reflect.KClass
 
-abstract class SerializationTest<out T : Any> (private val type: KClass<T>) {
+abstract class SerializationTest<out T : Any>(private val type: KClass<T>) {
     abstract val testObjects: List<T>
 
     // Some formats are excluded because they don't support all features
     private val ignoredFormats: Set<SerializationFormat> = setOf(Csv)
 
-    @Test fun `Object is mapped and parsed back without error` () {
+    @Test fun `Object is mapped and parsed back without error`() {
         (formats - ignoredFormats).forEach { contentType ->
             testObjects.forEach {
-                val map = it.convertToMap ()
+                val map = it.convertToMap()
 
-                val object2 = map.convertToObject (type)
+                val object2 = map.convertToObject(type)
                 assert(it == object2)
+                assert(it !== object2)
 
                 val modelString = it.serialize(contentType)
                 assert(modelString == it.serialize(contentType.contentType))
                 val object3 = modelString.parse(type, contentType)
                 assert(it == object3)
+                assert(it !== object3)
 
                 assert(modelString.parse(contentType) == map)
 
