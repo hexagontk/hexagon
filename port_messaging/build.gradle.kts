f42cdca052ab36b5550c16be366c500be7e17ec5
Wesley <wesley.herculano@ccc.ufcg.edu.br>
2019-10-09 11:54:47 -0300

    Port gradle to kotlin of templates_pebble, store_mongodb, port_templates, port_messaging, port_http_client, messaging_rabitmq

---
+++ port_messaging/build.gradle.kts
@@ -0,0 +1,9 @@
+
+apply(from = "../gradle/kotlin.gradle")
+apply(from = "../gradle/bintray.gradle")
+apply(from = "../gradle/dokka.gradle")
+apply(from = "../gradle/testng.gradle")
+
+dependencies {
+    api(project(":hexagon_core"))
+}
