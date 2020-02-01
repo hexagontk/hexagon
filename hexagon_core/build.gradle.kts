59be33b30d1abb6c2a61940cace4708fc161537f
Juanjo Aguililla <juanjoaguililla@gmail.com>
2020-02-01 19:51:58 +0100

    Test SonarQube settings

--- hexagon_core/build.gradle.kts
+++ hexagon_core/build.gradle.kts
@@ -3,7 +3,6 @@ apply(from = "../gradle/kotlin.gradle")
 apply(from = "../gradle/bintray.gradle")
 apply(from = "../gradle/dokka.gradle")
 apply(from = "../gradle/testng.gradle")
-apply(from = "../gradle/sonarqube.gradle")
 
 dependencies {
     val slf4jVersion = properties["slf4jVersion"]
