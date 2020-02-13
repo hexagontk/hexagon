06b38c604dccb086542f4ec641f651fcf2745e9d
Juanjo Aguililla <juanjoaguililla@gmail.com>
2020-02-13 19:25:14 +0100

    Hack to run apparently ignored tests

---
+++ hexagon_starters/src/test/kotlin/com/hexagonkt/starter/ServiceTest.kt
@@ -0,0 +1,39 @@
+package com.hexagonkt.starter
+
+import com.hexagonkt.http.client.Client
+import com.hexagonkt.http.client.ahc.AhcAdapter
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import server
+import main
+
+/**
+ * TODO Required because SonarQube doesn't seem to run tests without package.
+ */
+@TestInstance(PER_CLASS)
+class ServiceTest {
+    private val client: Client by lazy {
+        Client(AhcAdapter(), "http://localhost:${server.runtimePort}")
+    }
+
+    @BeforeAll fun startup() {
+        main()
+    }
+
+    @AfterAll fun shutdown() {
+        server.stop()
+    }
+
+    @Test fun `HTTP request returns the correct body`() {
+        val response = client.get("/hello/World")
+        val content = response.body
+
+        assert(response.headers["Date"] != null)
+        assert(response.headers["Content-Type"]?.first() == "text/plain")
+
+        assert("Hello, World!" == content)
+    }
+}
