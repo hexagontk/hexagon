7f154ca348123d6080290a3abbddd0f2dcb6cfe9
Juanjo Aguililla <juanjoaguililla@gmail.com>
2020-03-04 22:44:11 +0100

    New version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ rootProject.name = "hexagon"
 gradle.rootProject {
 
     allprojects {
-        version = "1.2.9"
+        version = "1.2.10"
         group = "com.hexagonkt"
         description = "The atoms of your platform"
 
