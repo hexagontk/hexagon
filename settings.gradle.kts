71112ecc3846479a9b82d86d5d185c9997f9e695
jaguililla <juanjoaguililla@gmail.com>
2020-05-23 14:16:54 +0200

    Set next release version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ rootProject.name = "hexagon"
 gradle.rootProject {
 
     allprojects {
-        version = "1.2.17"
+        version = "1.2.18"
         group = "com.hexagonkt"
         description = "The atoms of your platform"
     }
