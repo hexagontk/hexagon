293ef9c3f79d307c8ba00437bad72c6e621367e9
jaguililla <juanjoaguililla@gmail.com>
2020-08-05 18:59:57 +0200

    Upgrade dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    val jacksonVersion = "2.11.1"
+    val jacksonVersion = "2.11.2"
     val junitVersion = "5.6.2"
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
