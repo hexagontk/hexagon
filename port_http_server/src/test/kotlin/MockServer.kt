ca35c86f61a308b029f5bf4e47fa86455ad5ae59
Bikramjeet Singh <singhbikramjeet@outlook.com>
2020-08-15 18:11:13 +0530

    Improve parameter verification and response fetch
    * Add ability to fetch example response from
    schema in addition to from mediatype object
    * When verifying parameters, also check for
    valid enum values
    * Also add some explicit types and named
    parameters for better readability

--- port_http_server/src/test/kotlin/MockServer.kt
+++ port_http_server/src/test/kotlin/MockServer.kt
@@ -1,8 +1,11 @@
 package com.hexagonkt.http.server
 
+import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.Operation
 import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.media.MediaType
 import io.swagger.v3.oas.models.parameters.Parameter
+import io.swagger.v3.oas.models.responses.ApiResponse
 import io.swagger.v3.parser.OpenAPIV3Parser
 
 class MockServer(pathToSpec: String) {
@@ -10,7 +13,7 @@ class MockServer(pathToSpec: String) {
     val server: Server by lazy { createServer() }
 
     private val openAPIParser = OpenAPIV3Parser()
-    private val openAPISpec = openAPIParser.read(pathToSpec) ?: throw IllegalArgumentException()
+    private val openAPISpec: OpenAPI = openAPIParser.read(pathToSpec) ?: throw IllegalArgumentException()
 
     private fun createServer() = Server {
         openAPISpec.paths.forEach { path: String, pathItem: PathItem ->
@@ -19,39 +22,39 @@ class MockServer(pathToSpec: String) {
                     handleRequest(getOperation, call = this)
                 }
             }
-            pathItem.head?.let {
+            pathItem.head?.let { headOperation ->
                 this.head(path = path) {
-                    handleRequest(it, call = this)
+                    handleRequest(headOperation, call = this)
                 }
             }
-            pathItem.post?.let {
+            pathItem.post?.let { postOperation ->
                 this.post(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(postOperation, call = this)
                 }
             }
-            pathItem.put?.let {
+            pathItem.put?.let { putOperation ->
                 this.put(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(putOperation, call = this)
                 }
             }
-            pathItem.delete?.let {
+            pathItem.delete?.let { deleteOperation ->
                 this.delete(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(deleteOperation, call = this)
                 }
             }
-            pathItem.trace?.let {
+            pathItem.trace?.let { traceOperation ->
                 this.trace(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(traceOperation, call = this)
                 }
             }
-            pathItem.options?.let {
+            pathItem.options?.let { optionsOperation ->
                 this.options(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(optionsOperation, call = this)
                 }
             }
-            pathItem.patch?.let {
+            pathItem.patch?.let { patchOperation ->
                 this.patch(path = path) {
-                    handleRequest(it, this)
+                    handleRequest(patchOperation, call = this)
                 }
             }
         }
@@ -59,23 +62,28 @@ class MockServer(pathToSpec: String) {
 
     private fun handleRequest(operation: Operation, call: Call) {
         verifyParams(operation, call)
-        call.ok(content = getResponseContentForStatus(operation, 200))
+        call.ok(content = getResponseContentForStatus(operation, status = 200))
     }
 
     private fun getResponseContentForStatus(operation: Operation, status: Int): String {
-        val responseForStatus = operation.responses[status.toString()] ?: throw Exception()
-        val jsonResponses = responseForStatus.content["application/json"] ?: throw Exception()
-        return jsonResponses.example.toString()
+        val responsesForStatus: ApiResponse = operation.responses[status.toString()] ?: throw Exception("responses missing")
+        val jsonResponses: MediaType = responsesForStatus.content["application/json"] ?: throw Exception("responses for status missing")
+        val exampleResponse: Any? = getExampleFromSchema(jsonResponses) ?: getExampleFromMediaType(jsonResponses)
+        return exampleResponse?.toString() ?: ""
     }
 
+    private fun getExampleFromSchema(mediaType: MediaType) = mediaType.schema?.example
+
+    private fun getExampleFromMediaType(mediaType: MediaType) = mediaType.example
+
     private fun verifyParams(operation: Operation, call: Call) {
-        operation.parameters.forEach { parameter ->
+        operation.parameters?.forEach { parameter ->
             when (parameter.`in`) {
                 "path" -> {
                     if (!verifyPathParam(parameter, call)) {
                         call.halt(
                             code = 400,
-                            content = operation.responses["400"]?.description ?: ""
+                            content = getResponseContentForStatus(operation, status = 400)
                         )
                     }
                 }
@@ -83,7 +91,7 @@ class MockServer(pathToSpec: String) {
                     if (!verifyQueryParam(parameter, call)) {
                         call.halt(
                             code = 400,
-                            content = operation.responses["400"]?.description ?: ""
+                            content = getResponseContentForStatus(operation, status = 400)
                         )
                     }
                 }
@@ -91,7 +99,7 @@ class MockServer(pathToSpec: String) {
                     if (!verifyHeaderParam(parameter, call)) {
                         call.halt(
                             code = 400,
-                            content = operation.responses["400"]?.description ?: ""
+                            content = getResponseContentForStatus(operation, status = 400)
                         )
                     }
                 }
@@ -99,7 +107,7 @@ class MockServer(pathToSpec: String) {
                     if (!verifyCookieParam(parameter, call)) {
                         call.halt(
                             code = 400,
-                            content = operation.responses["400"]?.description ?: ""
+                            content = getResponseContentForStatus(operation, status = 400)
                         )
                     }
                 }
@@ -111,36 +119,64 @@ class MockServer(pathToSpec: String) {
      * Verifies a single path parameter. Returns false if the following conditions are fulfilled:
      * 1. Parameter is required as per the OpenAPI Spec
      * 2. No value is passed for the parameter in the Request
+     * 3. The value passed is not in the list of valid values as per the OpenAPI Spec
      * Returns true in all other cases.
      */
-    private fun verifyPathParam(parameter: Parameter, call: Call): Boolean =
-        !(parameter.required && call.request.pathParameters[parameter.name] == null)
+    private fun verifyPathParam(parameter: Parameter, call: Call): Boolean {
+        if (!parameter.required) return true
+        if (call.request.pathParameters[parameter.name].isNullOrBlank()) return false
+        parameter.schema.enum?.let {
+            if (call.request.pathParameters[parameter.name] !in it) return false
+        }
+        return true
+    }
 
     /**
      * Verifies a single query parameter. Returns false if the following conditions are fulfilled:
      * 1. Parameter is required as per the OpenAPI Spec
      * 2. No value is passed for the parameter in the Request
+     * 3. The value passed is not in the list of valid values as per the OpenAPI Spec
      * Returns true in all other cases.
      */
-    private fun verifyQueryParam(parameter: Parameter, call: Call): Boolean =
-        !(parameter.required && call.request.queryParameters[parameter.name] == null)
+    private fun verifyQueryParam(parameter: Parameter, call: Call): Boolean {
+        if (!parameter.required) return true
+        if (call.request.queryParameters[parameter.name].isNullOrBlank()) return false
+        parameter.schema.enum?.let {
+            if (call.request.queryParameters[parameter.name] !in it) return false
+        }
+        return true
+    }
 
     /**
      * Verifies a single header parameter. Returns false if the following conditions are fulfilled:
      * 1. Parameter is required as per the OpenAPI Spec
      * 2. No value is passed for the parameter in the Request
+     * 3. The value passed is not in the list of valid values as per the OpenAPI Spec
      * Returns true in all other cases.
      */
-    private fun verifyHeaderParam(parameter: Parameter, call: Call): Boolean =
-        !(parameter.required && call.request.headers[parameter.name] == null)
+    private fun verifyHeaderParam(parameter: Parameter, call: Call): Boolean {
+        if (!parameter.required) return true
+        if (call.request.headers[parameter.name].isNullOrBlank()) return false
+        parameter.schema.enum?.let {
+            if (call.request.headers[parameter.name] !in it) return false
+        }
+        return true
+    }
 
     /**
      * Verifies a single cookie parameter. Returns false if the following conditions are fulfilled:
      * 1. Parameter is required as per the OpenAPI Spec
      * 2. No value is passed for the parameter in the Request
+     * 3. The value passed is not in the list of valid values as per the OpenAPI Spec
      * Returns true in all other cases.
      */
-    private fun verifyCookieParam(parameter: Parameter, call: Call): Boolean =
-        !(parameter.required && call.request.cookies[parameter.name] == null)
+    private fun verifyCookieParam(parameter: Parameter, call: Call): Boolean {
+        if (!parameter.required) return true
+        if (call.request.cookies[parameter.name] == null) return false
+        parameter.schema.enum?.let {
+            if (call.request.cookies[parameter.name] !in it) return false
+        }
+        return true
+    }
 
 }
