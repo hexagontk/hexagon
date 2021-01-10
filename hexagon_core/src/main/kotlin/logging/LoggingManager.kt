fef5a70bff59dc0f5b77d8cda123063e00baf031
jaguililla <juanjoaguililla@gmail.com>
2021-01-10 18:05:40 +0100

    Improve log handling
    * Allow adapter injection
    * Improve example setting files (Logback)
    * Update JUL configuration instead overriding it

--- hexagon_core/src/main/kotlin/logging/LoggingManager.kt
+++ hexagon_core/src/main/kotlin/logging/LoggingManager.kt
@@ -1,7 +1,8 @@
 package com.hexagonkt.logging
 
+import com.hexagonkt.injection.InjectionManager.injectOrNull
 import com.hexagonkt.logging.jul.JulLoggingAdapter
 
 object LoggingManager {
-    var adapter: LoggingPort = JulLoggingAdapter
+    var adapter: LoggingPort = injectOrNull() ?: JulLoggingAdapter
 }
