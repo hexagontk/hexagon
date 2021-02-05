edebbb25fc9d687a96759e115507138180ee627b
jaguililla <juanjoaguililla@gmail.com>
2021-02-05 20:29:09 +0100

    Support programmatic logger level change

--- hexagon_core/src/main/kotlin/logging/jul/JulLoggingAdapter.kt
+++ hexagon_core/src/main/kotlin/logging/jul/JulLoggingAdapter.kt
@@ -1,38 +1,26 @@
 package com.hexagonkt.logging.jul
 
-import com.hexagonkt.ClasspathHandlerProvider
-import com.hexagonkt.ResourceNotFoundException
 import com.hexagonkt.logging.LoggerPort
 import com.hexagonkt.logging.LoggingLevel
 import com.hexagonkt.logging.LoggingLevel.*
 import com.hexagonkt.logging.LoggingPort
-import java.net.URL
 import java.util.logging.*
-import java.util.logging.LogManager.getLogManager
 import java.util.logging.Logger as JulLogger
 
 object JulLoggingAdapter : LoggingPort {
 
-    private const val defaultLoggingProperties = "classpath:logging.properties"
+    private val root = JulLogger.getLogger("")
 
     init {
-        // Register classpath handler manually because of an SPI error in Kotlin scripts
-        ClasspathHandlerProvider.registerHandler()
-
-        val root = JulLogger.getLogger("")
         for (hnd in root.handlers)
             root.removeHandler(hnd)
 
         root.addHandler(SystemOutHandler())
         root.level = Level.INFO
+    }
 
-        try {
-            getLogManager().updateConfiguration(URL(defaultLoggingProperties).openStream(), null)
-        }
-        catch (e: ResourceNotFoundException) {
-            val logger = JulLogger.getLogger(this::class.java.name)
-            logger.info("Default JUL configuration file ('$defaultLoggingProperties') not found")
-        }
+    override fun setLoggerLevel(name: String, level: LoggingLevel) {
+        JulLogger.getLogger(name).level = mapLevel(level)
     }
 
     override fun createLogger(name: String): LoggerPort =
@@ -54,13 +42,13 @@ object JulLoggingAdapter : LoggingPort {
                 if (log.isLoggable(julLevel))
                     log.log(julLevel, message(exception).toString(), exception)
             }
-
-            private fun mapLevel(level: LoggingLevel) = when (level) {
-                TRACE -> Level.FINER
-                DEBUG -> Level.FINE
-                INFO -> Level.INFO
-                WARN -> Level.WARNING
-                ERROR -> Level.SEVERE
-            }
         }
+
+    private fun mapLevel(level: LoggingLevel) = when (level) {
+        TRACE -> Level.FINER
+        DEBUG -> Level.FINE
+        INFO -> Level.INFO
+        WARN -> Level.WARNING
+        ERROR -> Level.SEVERE
+    }
 }
