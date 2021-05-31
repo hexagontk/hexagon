d48c088425694497dea5d0907da33628a2c91d40
jaguililla <juanjoaguililla@gmail.com>
2021-05-31 20:08:56 +0200

    Add basic Emoji support

---
+++ hexagon_core/src/main/kotlin/helpers/Emoji.kt
@@ -0,0 +1,17 @@
+package com.hexagonkt.helpers
+
+/**
+ * TODO Add other Emoji groups. Check: http://www.unicode.org/emoji/charts/full-emoji-list.html
+ */
+object Emoji {
+    object FaceCostume {
+        const val POO = "\uD83D\uDCA9"
+        const val CLOWN = "\uD83E\uDD21"
+        const val OGRE = "\uD83D\uDC79"
+        const val GOBLIN = "\uD83D\uDC7A"
+        const val GHOST = "\uD83D\uDC7B"
+        const val ALIEN = "\uD83D\uDC7D"
+        const val ALIEN_MONSTER = "\uD83D\uDC7E"
+        const val ROBOT = "\uD83E\uDD16"
+    }
+}
