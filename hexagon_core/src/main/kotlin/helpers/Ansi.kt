015c3ae578d1ec00690e4ff976d069924aeabd9e
jaguililla <juanjoaguililla@gmail.com>
2021-07-11 14:07:31 +0200

    Add documentation stubs

--- hexagon_core/src/main/kotlin/helpers/Ansi.kt
+++ hexagon_core/src/main/kotlin/helpers/Ansi.kt
@@ -7,53 +7,96 @@ package com.hexagonkt.helpers
  * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
  */
 object Ansi {
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val RESET = "\u001B[0m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLACK = "\u001B[30m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val RED = "\u001B[31m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val GREEN = "\u001B[32m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val YELLOW = "\u001B[33m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLUE = "\u001B[34m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val MAGENTA = "\u001B[35m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val CYAN = "\u001B[36m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val WHITE = "\u001B[37m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val DEFAULT = "\u001B[39m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLACK_BG = "\u001B[40m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val RED_BG = "\u001B[41m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val GREEN_BG = "\u001B[42m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val YELLOW_BG = "\u001B[43m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLUE_BG = "\u001B[44m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val MAGENTA_BG = "\u001B[45m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val CYAN_BG = "\u001B[46m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val WHITE_BG = "\u001B[47m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val DEFAULT_BG = "\u001B[49m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_BLACK = "\u001B[90m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_RED = "\u001B[91m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_GREEN = "\u001B[92m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_YELLOW = "\u001B[93m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_BLUE = "\u001B[94m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_MAGENTA = "\u001B[95m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_CYAN = "\u001B[96m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_WHITE = "\u001B[97m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_BLACK_BG = "\u001B[100m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_RED_BG = "\u001B[101m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_GREEN_BG = "\u001B[102m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_YELLOW_BG = "\u001B[103m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_BLUE_BG = "\u001B[104m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_MAGENTA_BG = "\u001B[105m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_CYAN_BG = "\u001B[106m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BRIGHT_WHITE_BG = "\u001B[107m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BOLD = "\u001B[1m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val UNDERLINE = "\u001B[4m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLINK = "\u001B[5m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val INVERSE = "\u001B[7m"
 
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BOLD_OFF = "\u001B[21m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val UNDERLINE_OFF = "\u001B[24m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val BLINK_OFF = "\u001B[25m"
+    /** [TODO](https://github.com/hexagonkt/hexagon/issues/271). */
     const val INVERSE_OFF = "\u001B[27m"
 }
