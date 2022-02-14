20618aee6fd5a0e22047d8d2fca9490e3c62d764
jaguililla <juanjoaguililla@gmail.com>
2022-02-14 18:59:43 +0100

    Modularize components

---
+++ serialization_jackson_xml/src/main/kotlin/module-info.java
@@ -0,0 +1,8 @@
+
+module com.hexagonkt.serialization.jackson.xml {
+
+    requires transitive com.hexagonkt.core;
+    requires transitive com.hexagonkt.serialization;
+
+    exports com.hexagonkt.serialization.jackson.xml;
+}
