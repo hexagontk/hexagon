a07ab9aa9aeb3f3d750463a60af5ba114576dc20
jaguililla <juanjoaguililla@gmail.com>
2022-03-26 16:46:03 +0100

    :Fix error when serving files with no known media type

--- http_server/src/test/kotlin/com/hexagonkt/http/server/callbacks/FileCallbackTest.kt
+++ http_server/src/test/kotlin/com/hexagonkt/http/server/callbacks/FileCallbackTest.kt
@@ -1,15 +1,21 @@
 package com.hexagonkt.http.server.callbacks
 
 import com.hexagonkt.core.handlers.Context
+import com.hexagonkt.core.media.TextMedia
+import com.hexagonkt.http.model.ContentType
+import com.hexagonkt.http.model.SuccessStatus
 import com.hexagonkt.http.patterns.LiteralPathPattern
+import com.hexagonkt.http.patterns.TemplatePathPattern
 import com.hexagonkt.http.server.handlers.HttpServerContext
 import com.hexagonkt.http.server.handlers.HttpServerPredicate
 import com.hexagonkt.http.server.model.HttpServerCall
+import com.hexagonkt.http.server.model.HttpServerRequest
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.lang.IllegalStateException
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
 
 internal class FileCallbackTest {
 
@@ -24,5 +30,47 @@ internal class FileCallbackTest {
         }
 
         assertEquals("File loading require a single path parameter", e.message)
+    }
+
+    @Test fun `Return file without known media type`() {
+        val callback = FileCallback(File("."))
+        val result = callback(
+            HttpServerContext(
+                Context(
+                    HttpServerCall(
+                        HttpServerRequest(
+                            path = "/build.gradle.kts"
+                        )
+                    ),
+                    HttpServerPredicate(
+                        pathPattern = TemplatePathPattern("/*")
+                    )
+                )
+            )
+        ).response
+
+        assertEquals(SuccessStatus.OK, result.status)
+        assertNull(result.contentType)
+    }
+
+    @Test fun `Return file with known media type`() {
+        val callback = FileCallback(File("."))
+        val result = callback(
+            HttpServerContext(
+                Context(
+                    HttpServerCall(
+                        HttpServerRequest(
+                            path = "/README.md"
+                        )
+                    ),
+                    HttpServerPredicate(
+                        pathPattern = TemplatePathPattern("/*")
+                    )
+                )
+            )
+        ).response
+
+        assertEquals(SuccessStatus.OK, result.status)
+        assertEquals(ContentType(TextMedia.MARKDOWN), result.contentType)
     }
 }
