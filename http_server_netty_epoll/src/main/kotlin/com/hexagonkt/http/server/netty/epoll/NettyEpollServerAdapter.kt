79419e2e9d744561a066d3eeaa35fb59adc84755
jaguililla <juanjoaguililla@gmail.com>
2022-04-28 22:41:04 +0200

    :Add Netty epoll :feature

---
+++ http_server_netty_epoll/src/main/kotlin/com/hexagonkt/http/server/netty/epoll/NettyEpollServerAdapter.kt
@@ -0,0 +1,49 @@
+package com.hexagonkt.http.server.netty.epoll
+
+import com.hexagonkt.core.Jvm
+import com.hexagonkt.http.server.HttpServerPort
+import com.hexagonkt.http.server.netty.NettyServerAdapter
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.Channel
+import io.netty.channel.ChannelOption
+import io.netty.channel.MultithreadEventLoopGroup
+import io.netty.channel.epoll.EpollChannelOption
+import io.netty.channel.epoll.EpollEventLoopGroup
+import io.netty.channel.epoll.EpollServerSocketChannel
+
+/**
+ * Implements [HttpServerPort] using Netty Epoll [Channel].
+ */
+class NettyEpollServerAdapter(
+    bossGroupThreads: Int = 1,
+    workerGroupThreads: Int = 0,
+    executorThreads: Int = Jvm.cpuCount * 2,
+    private val soBacklog: Int = 4 * 1_024,
+    private val soReuseAddr: Boolean = true,
+    private val soKeepAlive: Boolean = true,
+) : NettyServerAdapter(
+    bossGroupThreads,
+    workerGroupThreads,
+    executorThreads,
+    soBacklog,
+    soReuseAddr,
+    soKeepAlive,
+) {
+
+    constructor() : this(1, 0, Jvm.cpuCount * 2, 4 * 1_024, true, true)
+
+    override fun groupSupplier(it: Int): MultithreadEventLoopGroup =
+        EpollEventLoopGroup(it)
+
+    override fun serverBootstrapSupplier(
+        bossGroup: MultithreadEventLoopGroup,
+        workerGroup: MultithreadEventLoopGroup,
+    ): ServerBootstrap =
+        ServerBootstrap().group(bossGroup, workerGroup)
+            .channel(EpollServerSocketChannel::class.java)
+            .option(EpollChannelOption.SO_REUSEPORT, true)
+            .option(ChannelOption.SO_BACKLOG, soBacklog)
+            .option(ChannelOption.SO_REUSEADDR, soReuseAddr)
+            .childOption(ChannelOption.SO_KEEPALIVE, soKeepAlive)
+            .childOption(ChannelOption.SO_REUSEADDR, soReuseAddr)
+}
---
+++ http_server_netty_epoll/src/test/kotlin/com/hexagonkt/http/server/netty/epoll/AdapterExamplesTest.kt
@@ -0,0 +1,29 @@
+package com.hexagonkt.http.server.netty.epoll
+
+import com.hexagonkt.http.client.jetty.JettyClientAdapter
+import com.hexagonkt.http.test.examples.*
+import com.hexagonkt.serialization.jackson.JacksonTextFormat
+import com.hexagonkt.serialization.jackson.json.Json
+import com.hexagonkt.serialization.jackson.yaml.Yaml
+
+// TODO Fix disabled test
+// TODO Assert context methods (request.method, request.protocol...)
+// TODO Check response headers don't contain invalid chars (\n, \t...)
+
+val clientAdapter: () -> JettyClientAdapter = ::JettyClientAdapter
+val serverAdapter: () -> NettyEpollServerAdapter = ::NettyEpollServerAdapter
+val formats: List<JacksonTextFormat> = listOf(Json, Yaml)
+
+internal class AdapterBooksTest : BooksTest(clientAdapter, serverAdapter)
+internal class AdapterErrorsTest : ErrorsTest(clientAdapter, serverAdapter)
+internal class AdapterFiltersTest : FiltersTest(clientAdapter, serverAdapter)
+internal class AdapterClientTest : ClientTest(clientAdapter, serverAdapter, formats)
+internal class AdapterHttpsTest : HttpsTest(clientAdapter, serverAdapter)
+internal class AdapterZipTest : ZipTest(clientAdapter, serverAdapter)
+internal class AdapterCookiesTest : CookiesTest(clientAdapter, serverAdapter)
+internal class AdapterFilesTest : FilesTest(clientAdapter, serverAdapter)
+internal class AdapterCorsTest : CorsTest(clientAdapter, serverAdapter)
+internal class AdapterSamplesTest : SamplesTest(clientAdapter, serverAdapter)
+internal class AdapterBenchmarkIT : BenchmarkIT(clientAdapter, serverAdapter)
+//@Disabled
+//internal class AdapterAsyncSseTest : SseTest(clientAdapter, serverAdapter, async)
