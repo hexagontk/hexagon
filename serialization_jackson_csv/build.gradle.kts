d5fb56831b5deee3ee59839fbb022045c8293cbd
jaguililla <juanjoaguililla@gmail.com>
2022-05-08 22:34:48 +0200

    Fix dependencies problems

--- serialization_jackson_csv/build.gradle.kts
+++ serialization_jackson_csv/build.gradle.kts
@@ -11,6 +11,9 @@ apply(from = "../gradle/detekt.gradle")
 description = "Hexagon CSV serialization format (using Jackson)."
 
 dependencies {
+    val jacksonVersion = properties["jacksonVersion"]
+
     api(project(":serialization_jackson"))
+    api(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
 }
--- serialization_jackson_xml/build.gradle.kts
+++ serialization_jackson_xml/build.gradle.kts
@@ -11,6 +11,9 @@ apply(from = "../gradle/detekt.gradle")
 description = "Hexagon XML serialization format (using Jackson)."
 
 dependencies {
+    val jacksonVersion = properties["jacksonVersion"]
+
     api(project(":serialization_jackson"))
+    api(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 }
