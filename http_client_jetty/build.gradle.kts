f6df8d162d725d8c9995c6b0f0fd657cbdcb069d
jaguililla <juanjoaguililla@gmail.com>
2022-06-28 21:12:06 +0200

    Improve dependencies

--- http_client_jetty/build.gradle.kts
+++ http_client_jetty/build.gradle.kts
@@ -12,7 +12,9 @@ dependencies {
 
     "api"(project(":http_client"))
     "api"(platform("org.eclipse.jetty:jetty-bom:$jettyVersion"))
-    "api"("org.eclipse.jetty:jetty-client")
+    "api"("org.eclipse.jetty:jetty-client") {
+        exclude(group = "org.slf4j")
+    }
 
     "testImplementation"("org.eclipse.jetty.websocket:websocket-jetty-client")
 }
