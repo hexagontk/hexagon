e0ef8962a2a67ef443f5f36a3720f2b39fc3955c
jaguililla <juanjoaguililla@gmail.com>
2022-08-16 13:01:34 +0200

    Add locale utilities

--- core/src/main/kotlin/com/hexagonkt/core/Helpers.kt
+++ core/src/main/kotlin/com/hexagonkt/core/Helpers.kt
@@ -4,13 +4,10 @@ import com.hexagonkt.core.logging.Logger
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
-import java.lang.IllegalArgumentException
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.Socket
-import java.util.*
 import java.util.concurrent.TimeUnit.SECONDS
-import kotlin.reflect.KClass
 
 /**
  *  Disable heavy and optional checks in runtime. This flag can be enabled to get a small
@@ -25,21 +22,6 @@ import kotlin.reflect.KClass
  */
 var disableChecks: Boolean = Jvm.systemFlag("DISABLE_CHECKS")
 
-/** Set of allowed country codes in this JVM. */
-val countryCodes: Set<String> by lazy {
-    Locale.getISOCountries().toSet()
-}
-
-/** Set of allowed language codes in this JVM. */
-val languageCodes: Set<String> by lazy {
-    Locale.getISOLanguages().toSet()
-}
-
-/** Set of allowed currency codes in this JVM. */
-val currencyCodes: Set<String> by lazy {
-    Currency.getAvailableCurrencies().map { it.currencyCode }.toSet()
-}
-
 private val logger: Logger by lazy { Logger("com.hexagonkt.core.Helpers") }
 
 /**
@@ -51,58 +33,6 @@ private val logger: Logger by lazy { Logger("com.hexagonkt.core.Helpers") }
  */
 fun <T> T.println(prefix: String = ""): T =
     apply { kotlin.io.println("$prefix$this") }
-
-/**
- * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
- *
- * @param locale .
- * @return .
- */
-inline fun <reified T : ResourceBundle> resourceBundle(
-    locale: Locale = Locale.getDefault()): ResourceBundle =
-        resourceBundle(T::class, locale)
-
-/**
- * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
- *
- * @param type .
- * @param locale .
- * @return .
- */
-fun <T : ResourceBundle> resourceBundle(
-    type: KClass<T>, locale: Locale = Locale.getDefault()): ResourceBundle =
-        ResourceBundle.getBundle(type.java.name, locale)
-
-/**
- * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
- *
- * @param language .
- * @param country .
- * @return .
- */
-fun localeOf(language: String = "", country: String = ""): Locale {
-    require(language.isNotEmpty() || country.isNotEmpty()) {
-        "A non-blank language or country is required"
-    }
-    require(language.isEmpty() || language in languageCodes) { "Language: '$language' not allowed" }
-    require(country.isEmpty() || country in countryCodes) { "Country: '$country' not allowed" }
-    return Locale(language, country)
-}
-
-/**
- * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
- *
- * @param language .
- * @param country .
- * @return .
- */
-fun localeOfOrNull(language: String = "", country: String = ""): Locale? =
-    try {
-        localeOf(language, country)
-    }
-    catch (_: IllegalArgumentException) {
-        null
-    }
 
 // NETWORK /////////////////////////////////////////////////////////////////////////////////////////
 /** Internet address used to bind services to all local network interfaces. */
---
+++ core/src/main/kotlin/com/hexagonkt/core/I18n.kt
@@ -0,0 +1,106 @@
+package com.hexagonkt.core
+
+import java.util.*
+import kotlin.reflect.KClass
+
+/** Set of allowed country codes in this JVM. */
+val countryCodes: Set<String> by lazy {
+    Locale.getISOCountries().toSet()
+}
+
+/** Set of allowed language codes in this JVM. */
+val languageCodes: Set<String> by lazy {
+    Locale.getISOLanguages().toSet()
+}
+
+/** Set of allowed currency codes in this JVM. */
+val currencyCodes: Set<String> by lazy {
+    Currency.getAvailableCurrencies().map { it.currencyCode }.toSet()
+}
+
+/**
+ * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
+ *
+ * @param locale .
+ * @return .
+ */
+inline fun <reified T : ResourceBundle> resourceBundle(
+    locale: Locale = Locale.getDefault()): ResourceBundle =
+    resourceBundle(T::class, locale)
+
+/**
+ * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
+ *
+ * @param type .
+ * @param locale .
+ * @return .
+ */
+fun <T : ResourceBundle> resourceBundle(
+    type: KClass<T>, locale: Locale = Locale.getDefault()): ResourceBundle =
+    ResourceBundle.getBundle(type.java.name, locale)
+
+/**
+ * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
+ *
+ * @param language .
+ * @param country .
+ * @return .
+ */
+fun localeOf(language: String = "", country: String = ""): Locale {
+    require(language.isNotEmpty() || country.isNotEmpty()) {
+        "A non-blank language or country is required"
+    }
+    require(language.isEmpty() || language in languageCodes) { "Language: '$language' not allowed" }
+    require(country.isEmpty() || country in countryCodes) { "Country: '$country' not allowed" }
+    return Locale(language, country)
+}
+
+/**
+ * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
+ *
+ * @param language .
+ * @param country .
+ * @return .
+ */
+fun localeOfOrNull(language: String = "", country: String = ""): Locale? =
+    try {
+        localeOf(language, country)
+    }
+    catch (_: IllegalArgumentException) {
+        null
+    }
+
+fun languageOf(language: String): Locale =
+    localeOf(language = language)
+
+fun languageOfOrNull(language: String): Locale? =
+    try {
+        languageOf(language)
+    }
+    catch (_: IllegalArgumentException) {
+        null
+    }
+
+fun countryOf(country: String): Locale =
+    localeOf(country = country)
+
+fun countryOfOrNull(country: String): Locale? =
+    try {
+        countryOf(country)
+    }
+    catch (_: IllegalArgumentException) {
+        null
+    }
+
+fun parseLocale(languageCountry: String): Locale =
+    languageCountry
+        .split("_")
+        .let { localeOf(it.getOrElse(0) { "" }, it.getOrElse(1) { "" }) }
+
+fun parseLocaleOrNull(languageCountry: String): Locale? =
+    try {
+        localeOf(languageCountry)
+    }
+    catch (_: IllegalArgumentException) {
+        null
+    }
