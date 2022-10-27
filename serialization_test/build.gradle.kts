dbbe6446b0e822bb2ad456e83f02bbd438072884
jaguililla <juanjoaguililla@gmail.com>
2022-10-27 07:35:57 +0200

    Complete build scripts

--- serialization_test/build.gradle.kts
+++ serialization_test/build.gradle.kts
@@ -7,6 +7,7 @@ apply(from = "../gradle/kotlin.gradle")
 apply(from = "../gradle/publish.gradle")
 apply(from = "../gradle/dokka.gradle")
 apply(from = "../gradle/detekt.gradle")
+apply(from = "../gradle/native.gradle")
 
 description = "Hexagon serialization testing helpers."
 
