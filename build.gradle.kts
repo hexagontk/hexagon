21fb62581ac28a3da338581d4c484d82d9476b4d
jaguililla <juanjoaguililla@gmail.com>
2022-12-20 20:04:17 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,7 @@ import com.github.jk1.license.render.ReportRenderer
  */
 
 plugins {
-//    kotlin("jvm") version("1.8.0") apply(false)
-    kotlin("jvm") version("1.8.0-RC") apply(false)
+    kotlin("jvm") version("1.8.0-RC2") apply(false)
 
     id("idea")
     id("eclipse")
