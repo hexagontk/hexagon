e0cfdcc512959ef2522c336e74f766d529bbf1c1
jaguililla <juanjoaguililla@gmail.com>
2022-12-30 13:27:01 +0100

    Remove GraalVM support

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ plugins {
     id("com.github.jk1.dependency-license-report") version("2.1")
     id("io.gitlab.arturbosch.detekt") version("1.22.0") apply(false)
     id("me.champeau.jmh") version("0.6.8") apply(false)
-    id("org.graalvm.buildtools.native") version("0.9.19") apply(false)
 }
 
 apply(from = "gradle/certificates.gradle")
--- http/build.gradle.kts
+++ http/build.gradle.kts
@@ -7,7 +7,6 @@ apply(from = "../gradle/kotlin.gradle")
 apply(from = "../gradle/publish.gradle")
 apply(from = "../gradle/dokka.gradle")
 apply(from = "../gradle/detekt.gradle")
-apply(from = "../gradle/native.gradle")
 
 description = "HTTP classes. These classes are shared among the HTTP client and the HTTP server."
 
