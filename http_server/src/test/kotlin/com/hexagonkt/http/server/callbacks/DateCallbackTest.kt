86ef9b3dde2c9eb17c9fea8df49f24c925000cb5
jaguililla <juanjoaguililla@gmail.com>
2023-01-31 21:09:19 +0100

    Fix test

--- http_server/src/test/kotlin/com/hexagonkt/http/server/callbacks/DateCallbackTest.kt
+++ http_server/src/test/kotlin/com/hexagonkt/http/server/callbacks/DateCallbackTest.kt
@@ -1,5 +1,6 @@
 package com.hexagonkt.http.server.callbacks
 
+import com.hexagonkt.core.fail
 import com.hexagonkt.core.require
 import com.hexagonkt.http.server.handlers.HttpServerContext
 import com.hexagonkt.http.toHttpFormat
@@ -10,8 +11,9 @@ import kotlin.test.assertEquals
 internal class DateCallbackTest {
 
     @Test fun `Date callback creates the proper response`() {
-        val instant = Instant.now().toHttpFormat()
-        val date = DateCallback()(HttpServerContext()).response.headers.require("date").value
-        assertEquals(instant, date)
+        val context = HttpServerContext()
+        val instant = Instant.now()
+        val date = DateCallback()(context).response.headers.require("date").value ?: fail
+        assertEquals(instant.toHttpFormat().substringBeforeLast(':'), date.substringBeforeLast(':'))
     }
 }
