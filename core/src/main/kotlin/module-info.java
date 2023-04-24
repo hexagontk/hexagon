7306ed6b96047e8737d586e4b4b6bdd8d7370099
jaguililla <juanjoaguililla@gmail.com>
2023-04-24 18:34:20 +0200

    Add module definitions

---
+++ core/src/main/kotlin/module-info.java
@@ -0,0 +1,13 @@
+
+module com.hexagonkt.core {
+
+    requires java.management;
+    requires transitive kotlin.stdlib;
+
+    exports com.hexagonkt.core;
+    exports com.hexagonkt.core.logging;
+    exports com.hexagonkt.core.media;
+    exports com.hexagonkt.core.security;
+
+    provides java.net.spi.URLStreamHandlerProvider with com.hexagonkt.core.ClasspathHandlerProvider;
+}
