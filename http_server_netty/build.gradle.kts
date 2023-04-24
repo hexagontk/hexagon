b18faf08b3350ee06f43e0253a059c7ce79416da
jaguililla <juanjoaguililla@gmail.com>
2023-04-24 13:43:07 +0200

    Debug native tests

--- http_server_netty/build.gradle.kts
+++ http_server_netty/build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     "api"("io.netty:netty-codec-http") { exclude(group = "org.slf4j") }
     "api"("io.netty:netty-codec-http2") { exclude(group = "org.slf4j") }
 
+    if (System.getProperty("os.name").lowercase().contains("mac"))
+        "api"("io.netty:netty-tcnative:2.0.60.Final:osx-x86_64") { exclude(group = "org.slf4j") }
+
     "testImplementation"(project(":http_test"))
     "testImplementation"(project(":http_client_jetty_ws"))
     "testImplementation"(project(":serialization_jackson_json"))
