cde670a636e125348ce960691fe01cb6d1ccd9e4
jaguililla <juanjoaguililla@gmail.com>
2023-05-02 15:55:47 +0200

    Delete failing module

--- http_server_netty/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module com.hexagonkt.http_server_netty {
-
-    requires transitive kotlin.stdlib;
-    requires transitive com.hexagonkt.http_handlers;
-    requires transitive com.hexagonkt.http_server;
-
-    exports com.hexagonkt.http.server.netty;
-}
