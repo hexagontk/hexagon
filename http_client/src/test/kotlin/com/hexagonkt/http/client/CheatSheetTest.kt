d93da10c568a0b8bacf7634d541aa643a5513802
jaguililla <juanjoaguililla@gmail.com>
2023-05-19 09:37:29 +0200

    Add cheat sheets (:wip)

---
+++ http_client/src/test/kotlin/com/hexagonkt/http/client/CheatSheetTest.kt
@@ -0,0 +1,8 @@
+package com.hexagonkt.http.client
+
+import org.junit.jupiter.api.Test
+
+internal class CheatSheetTest {
+
+    @Test fun `Client Cheat Sheet`() {}
+}
---
+++ http_server/src/test/kotlin/com/hexagonkt/http/server/CheatSheetTest.kt
@@ -0,0 +1,21 @@
+package com.hexagonkt.http.server
+
+import org.junit.jupiter.api.Test
+
+internal class CheatSheetTest {
+
+    @Test fun `Server Cheat Sheet`() {
+        // Basic server
+        // Settings
+        // Status (running port, started...)
+        // Handlers
+        //   As object tree
+        //   Using builder
+        //   On
+        //   After
+        //   Filter
+        //   Path
+        // Reusing handlers (use)
+        // Callbacks
+    }
+}
