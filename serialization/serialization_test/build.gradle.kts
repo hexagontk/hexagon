704499eb6a7dea58e08baad50f66ff3091a7d175
jaguililla <juanjoaguililla@gmail.com>
2023-06-19 18:05:24 +0200

    Disable not needed native tests

--- serialization/serialization_test/build.gradle.kts
+++ serialization/serialization_test/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 apply(from = "$rootDir/gradle/kotlin.gradle")
 apply(from = "$rootDir/gradle/publish.gradle")
 apply(from = "$rootDir/gradle/dokka.gradle")
-apply(from = "$rootDir/gradle/native.gradle")
 apply(from = "$rootDir/gradle/detekt.gradle")
 
 description = "Hexagon serialization testing helpers."
