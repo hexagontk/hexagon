60d4e7b3ff84cd7cc2c907a2ad7d6c91d1969b97
jaguililla <juanjoaguililla@gmail.com>
2023-08-09 16:49:18 +0200

    Delete module definitions

--- http/http_server_servlet/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module com.hexagonkt.http_server_servlet {
-
-    requires transitive kotlin.stdlib;
-    requires transitive com.hexagonkt.http;
-    requires transitive com.hexagonkt.http_server;
-
-    exports com.hexagonkt.http.server.servlet;
-}
