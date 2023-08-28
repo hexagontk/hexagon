b4c23eec33025b1fc9e5b975abdcb034326b8b12
jaguililla <juanjoaguililla@gmail.com>
2023-08-28 15:17:07 +0200

    Clean serialization module

--- serialization/serialization/src/main/kotlin/com/hexagonkt/serialization/Data.kt
+++ serialization/serialization/src/main/kotlin/com/hexagonkt/serialization/Data.kt
@@ -1,32 +1,6 @@
 package com.hexagonkt.serialization
 
-import kotlin.collections.Map.Entry
-
-interface Data<T> : Map<String, Any?> {
+interface Data<T> {
     fun data(): Map<String, *>
     fun with(data: Map<String, *>): T
-
-    override val entries: Set<Entry<String, Any?>>
-        get() = data().entries
-
-    override val keys: Set<String>
-        get() = data().keys
-
-    override val size: Int
-        get() = data().size
-
-    override val values: Collection<Any?>
-        get() = data().values
-
-    override fun isEmpty(): Boolean =
-        data().isEmpty()
-
-    override fun get(key: String): Any? =
-        data()[key]
-
-    override fun containsValue(value: Any?): Boolean =
-        data().containsValue(value)
-
-    override fun containsKey(key: String): Boolean =
-        data().containsKey(key)
 }
