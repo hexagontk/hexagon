8af67389f82de716aa00b39a3fb1de8ab13f9f64
jaguililla <juanjoaguililla@gmail.com>
2024-02-19 07:16:57 +0100

    Add Netty HTTP client :wip

---
+++ http/http_client_netty/README.md
@@ -0,0 +1,31 @@
+
+# Module http_client_netty
+[http_client] implementation using the [Netty HTTP Client] library.
+
+[http_client]: /http_client
+[Netty HTTP Client]: https://netty.io
+
+### Install the Dependency
+
+=== "build.gradle"
+
+    ```groovy
+    repositories {
+        mavenCentral()
+    }
+
+    implementation("com.hexagonkt:http_client_netty:$hexagonVersion")
+    ```
+
+=== "pom.xml"
+
+    ```xml
+    <dependency>
+      <groupId>com.hexagonkt</groupId>
+      <artifactId>http_client_netty</artifactId>
+      <version>$hexagonVersion</version>
+    </dependency>
+    ```
+
+# Package com.hexagonkt.http.client.netty
+Netty HTTP client implementation classes.
---
+++ http/http_client_netty/build.gradle.kts
@@ -0,0 +1,30 @@
+
+plugins {
+    id("java-library")
+}
+
+apply(from = "$rootDir/gradle/kotlin.gradle")
+apply(from = "$rootDir/gradle/publish.gradle")
+apply(from = "$rootDir/gradle/dokka.gradle")
+apply(from = "$rootDir/gradle/native.gradle")
+apply(from = "$rootDir/gradle/detekt.gradle")
+
+description = "HTTP client adapter for Jetty (without WebSockets support)."
+
+dependencies {
+    val nettyVersion = properties["nettyVersion"]
+//    val nettyTcNativeVersion = properties["nettyTcNativeVersion"]
+    val slf4jVersion = properties["slf4jVersion"]
+
+    "api"(project(":http:http_client"))
+    "api"("io.netty:netty-codec-http2:$nettyVersion") { exclude(group = "org.slf4j") }
+
+//    if (System.getProperty("os.name").lowercase().contains("mac"))
+//        "api"("io.netty:netty-tcnative:$nettyTcNativeVersion:osx-x86_64") {
+//            exclude(group = "org.slf4j")
+//        }
+
+    "testImplementation"("org.slf4j:log4j-over-slf4j:$slf4jVersion")
+    "testImplementation"("org.slf4j:jcl-over-slf4j:$slf4jVersion")
+    "testImplementation"("org.slf4j:slf4j-jdk14:$slf4jVersion")
+}
