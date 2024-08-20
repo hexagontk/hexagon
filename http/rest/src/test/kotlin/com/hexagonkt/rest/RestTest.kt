99f2422dcec70a7acc36b5b48ea7fd2e23b28db8
jaguililla <juanjoaguililla@gmail.com>
2024-08-20 16:26:14 +0200

    :Fix :test

--- http/rest/src/test/kotlin/com/hexagonkt/rest/RestTest.kt
+++ http/rest/src/test/kotlin/com/hexagonkt/rest/RestTest.kt
@@ -94,30 +94,35 @@ internal class RestTest {
     }
 
     @Test fun `Body is serialized`() {
-        HttpResponse(body = Record(0, 1, 2), contentType = jsonContentType).apply {
+        val body1 = mapOf("a" to 0, "b" to 1, "c" to 2)
+        HttpResponse(body = body1, contentType = jsonContentType).apply {
             assertEquals(APPLICATION_JSON, mediaType())
-            val serializedBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
-            assertEquals("""{"a":0,"b":1,"c":2}""", serializedBody)
+            val textBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
+            assertEquals("""{"a":0,"b":1,"c":2}""", textBody)
         }
-        HttpResponse(body = Record(3, 4, 5), contentType = yamlContentType).apply {
+        val body2 = mapOf("a" to 3, "b" to 4, "c" to 5)
+        HttpResponse(body = body2, contentType = yamlContentType).apply {
             assertEquals(APPLICATION_YAML, mediaType())
-            val serializedBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
-            assertEquals("""a:3b:4c:5""", serializedBody)
+            val textBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
+            assertEquals("""a:3b:4c:5""", textBody)
         }
-        HttpResponse(body = Record(6, 7, 8), contentType = xmlContentType).apply {
+        val body3 = mapOf("a" to 6, "b" to 7, "c" to 8)
+        HttpResponse(body = body3, contentType = xmlContentType).apply {
             assertEquals(APPLICATION_XML, mediaType())
-            val serializedBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
-            assertEquals("""<Record><a>6</a><b>7</b><c>8</c></Record>""", serializedBody)
+            val textBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
+            assertEquals("""<LinkedHashMap><a>6</a><b>7</b><c>8</c></LinkedHashMap>""", textBody)
         }
-        HttpResponse(body = Record(9, 10, 11), contentType = tomlContentType).apply {
+        val body4 = mapOf("a" to 9, "b" to 10, "c" to 11)
+        HttpResponse(body = body4, contentType = tomlContentType).apply {
             assertEquals(APPLICATION_TOML, mediaType())
-            val serializedBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
-            assertEquals("""a=9b=10c=11""", serializedBody)
+            val textBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
+            assertEquals("""a=9b=10c=11""", textBody)
         }
-        HttpResponse(body = listOf(12, 13, 14), contentType = csvContentType).apply {
+        val body5 = listOf(12, 13, 14)
+        HttpResponse(body = body5, contentType = csvContentType).apply {
             assertEquals(TEXT_CSV, mediaType())
-            val serializedBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
-            assertEquals("""12,13,14""", serializedBody)
+            val textBody = serializeBody().toString().replace(Regex("(\\s|\\n)*"), "")
+            assertEquals("""12,13,14""", textBody)
         }
     }
 }
--- serverless/serverless_http_google/src/main/kotlin/com/hexagonkt/serverless/http/google/GoogleServerlessHttpAdapter.kt
+++ serverless/serverless_http_google/src/main/kotlin/com/hexagonkt/serverless/http/google/GoogleServerlessHttpAdapter.kt
@@ -3,14 +3,28 @@ package com.hexagonkt.serverless.http.google
 import com.google.cloud.functions.HttpFunction
 import com.google.cloud.functions.HttpRequest
 import com.google.cloud.functions.HttpResponse
+import com.hexagonkt.http.handlers.HttpContext
 import com.hexagonkt.http.handlers.HttpHandler
 
 class GoogleServerlessHttpAdapter(val handler: HttpHandler): HttpFunction {
 
     override fun service(request: HttpRequest, response: HttpResponse) {
-        // Transform request
-        // Call handler
-        // Transform response
-        response.writer.write("Hello World!")
+        val handlerRequest = createRequest(request)
+        val handlerContext = handler.process(handlerRequest)
+        writeResponse(response, handlerContext)
+    }
+
+    private fun createRequest(request: HttpRequest): com.hexagonkt.http.model.HttpRequest {
+        TODO("Not yet implemented")
+    }
+
+    private fun writeResponse(response: HttpResponse, context: HttpContext) {
+        val handlerResponse = context.response
+
+        handlerResponse.contentType?.text?.let(response::setContentType)
+
+        response.headers // TODO
+        response.setStatusCode(handlerResponse.status.code)
+        response.writer.write(handlerResponse.bodyString())
     }
 }
