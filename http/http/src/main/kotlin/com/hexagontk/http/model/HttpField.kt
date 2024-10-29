2ebab2811a9e5ec77906c9fdec171ebc6f9b3f5b
jaguililla <juanjoaguililla@gmail.com>
2024-10-29 13:37:27 +0100

    Update

--- http/http/src/main/kotlin/com/hexagontk/http/model/HttpField.kt
+++ http/http/src/main/kotlin/com/hexagontk/http/model/HttpField.kt
@@ -2,8 +2,6 @@ package com.hexagontk.http.model
 
 /**
  * HTTP field single-value. Used in headers, query parameters and form parameters.
- *
- * TODO Use HttpFieldPort and rename Header to HttpField
  */
 interface HttpField {
     val name: String
