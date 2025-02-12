72a7b6d5ec790f0d0d3157ddec80606acea5ad39
jaguililla <juanjoaguililla@gmail.com>
2025-02-12 20:29:14 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ mapOf(
 apply(from = "gradle/certificates.gradle")
 
 allprojects {
-    version = "4.0.0"
+    version = "4.0.0-B5"
     group = "com.hexagontk"
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
             version("nativeTools", "0.10.5")
             version("jmhGradle", "0.7.2")
             version("gradleWrapper", "8.12.1")
-            version("mkdocsMaterial", "9.6.3")
+            version("mkdocsMaterial", "9.6.4")
             version("maven", "3.9.9")
             version("jreleaser", "1.15.0")
 
@@ -39,8 +39,8 @@ dependencyResolutionManagement {
             version("slf4j", "2.0.16")
 
             // http_server_netty
-            version("netty", "4.1.117.Final")
-            version("nettyTcNative", "2.0.69.Final")
+            version("netty", "4.1.118.Final")
+            version("nettyTcNative", "2.0.70.Final")
 
             // http_server_helidon
             version("helidon", "4.1.6")
