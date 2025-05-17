830958179ba4734c03f27d9892b231177e812650
jaguililla <juanjoaguililla@gmail.com>
2025-05-17 12:25:32 +0200

    Add module information

--- serverless/serverless_http_google/main/module-info.java
+++ serverless/serverless_http_google/main/module-info.java
@@ -3,7 +3,5 @@ module com.hexagontk.serverless_http_google {
 
     requires transitive com.hexagontk.http_handlers;
 
-    requires functions.framework.api;
-
     exports com.hexagontk.serverless.http.google;
 }
